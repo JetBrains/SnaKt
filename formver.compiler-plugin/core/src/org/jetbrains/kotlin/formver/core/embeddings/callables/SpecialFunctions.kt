c7f3166ef840853f0f6998d38eeb1f421167d9bc
Ramon.Wick <ramon.wick@jetbrains.com>
2026-02-20 16:41:24 +0100

    Added `havoc` function to `SpecialFunctions` with subtype postcondition. Update exprected viper dump.

--- formver.compiler-plugin/core/src/org/jetbrains/kotlin/formver/core/embeddings/callables/SpecialFunctions.kt
+++ formver.compiler-plugin/core/src/org/jetbrains/kotlin/formver/core/embeddings/callables/SpecialFunctions.kt
@@ -5,10 +5,20 @@
 
 package org.jetbrains.kotlin.formver.core.embeddings.callables
 
-import org.jetbrains.kotlin.formver.core.embeddings.expression.OperatorExpEmbeddings
 
+import org.jetbrains.kotlin.formver.core.domains.FunctionBuilder
+import org.jetbrains.kotlin.formver.core.domains.RuntimeTypeDomain
+import org.jetbrains.kotlin.formver.core.embeddings.expression.OperatorExpEmbeddings
+import org.jetbrains.kotlin.formver.viper.ast.Type
 
 object SpecialFunctions {
+
+    private val havoc =
+        FunctionBuilder.build("havoc") {
+            val t = argument( RuntimeTypeDomain.RuntimeType)
+            returns(Type.Ref)
+            postcondition { RuntimeTypeDomain.isSubtype(RuntimeTypeDomain.typeOf(result), t) }
+        }
     val all
-        get() = OperatorExpEmbeddings.allTemplates.map { it.refsOperation }
+        get() = OperatorExpEmbeddings.allTemplates.map { it.refsOperation } + havoc
 }
