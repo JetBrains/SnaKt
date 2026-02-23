8d5afbcde0efbb120ad44274d493e3f72d0189ba
marco <paganma@usi.ch>
2026-02-23 12:02:45 +0100

    Refactor uniqueness checker

--- formver.compiler-plugin/plugin/src/org/jetbrains/kotlin/formver/plugin/compiler/UniquenessDeclarationChecker.kt
+++ formver.compiler-plugin/plugin/src/org/jetbrains/kotlin/formver/plugin/compiler/UniquenessDeclarationChecker.kt
@@ -12,27 +12,32 @@ import org.jetbrains.kotlin.fir.analysis.checkers.MppCheckerKind
 import org.jetbrains.kotlin.fir.analysis.checkers.context.CheckerContext
 import org.jetbrains.kotlin.fir.analysis.checkers.declaration.FirSimpleFunctionChecker
 import org.jetbrains.kotlin.fir.declarations.FirSimpleFunction
+import org.jetbrains.kotlin.fir.resolve.dfa.controlFlowGraph
 import org.jetbrains.kotlin.formver.common.ErrorCollector
 import org.jetbrains.kotlin.formver.common.PluginConfiguration
-import org.jetbrains.kotlin.formver.uniqueness.UniqueCheckVisitor
-import org.jetbrains.kotlin.formver.uniqueness.UniqueChecker
 import org.jetbrains.kotlin.formver.uniqueness.UniquenessCheckException
+import org.jetbrains.kotlin.formver.uniqueness.UniquenessGraphChecker
 
-class UniqueDeclarationChecker(private val session: FirSession, private val config: PluginConfiguration) :
+class UniquenessDeclarationChecker(private val session: FirSession, private val config: PluginConfiguration) :
     FirSimpleFunctionChecker(MppCheckerKind.Common) {
 
     context(context: CheckerContext, reporter: DiagnosticReporter)
     override fun check(declaration: FirSimpleFunction) {
         if (!config.checkUniqueness) return
-        val errorCollector = ErrorCollector()
+        val out = ErrorCollector()
+
         try {
-            val uniqueCheckerContext = UniqueChecker(session, config, errorCollector)
-            declaration.accept(UniqueCheckVisitor, uniqueCheckerContext)
-        }
-        catch (e: UniquenessCheckException) {
+            val flowGraph = declaration.controlFlowGraphReference?.controlFlowGraph
+
+            if (flowGraph == null) {
+                throw IllegalStateException("Control flow graph is null for declaration: ${declaration.name}")
+            }
+
+            val graphChecker = UniquenessGraphChecker(session, out)
+            graphChecker.check(flowGraph)
+        } catch (e: UniquenessCheckException) {
             reporter.reportOn(e.source, PluginErrors.UNIQUENESS_VIOLATION, e.message)
-        }
-        catch (e: Exception) {
+        } catch (e: Exception) {
             val error = e.message ?: "No message provided"
             reporter.reportOn(declaration.source, PluginErrors.INTERNAL_ERROR, error)
         }
---
+++ formver.compiler-plugin/uniqueness/src/org/jetbrains/kotlin/formver/uniqueness/UniquenessGraphChecker.kt
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2010-2024 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.formver.uniqueness
+
+import org.jetbrains.kotlin.fir.FirSession
+import org.jetbrains.kotlin.fir.resolve.dfa.cfg.ControlFlowGraph
+import org.jetbrains.kotlin.formver.common.ErrorCollector
+
+class UniquenessGraphChecker(
+    val session: FirSession,
+    val out: ErrorCollector
+) {
+
+    fun check(graph: ControlFlowGraph) {
+    }
+
+}
