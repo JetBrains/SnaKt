55eb162fc1eba23def55e3d787c2bbb823408299
marco <paganma@usi.ch>
2026-03-03 09:50:44 +0100

    Reorganize tests

---
+++ formver.compiler-plugin/testData/diagnostics/uniqueness_checker/consume_property.kt
@@ -0,0 +1,64 @@
+// UNIQUE_CHECK_ONLY
+
+import org.jetbrains.kotlin.formver.plugin.Borrowed
+import org.jetbrains.kotlin.formver.plugin.Unique
+
+abstract class A {
+    @Unique abstract var x: Any
+    @Unique abstract var w: Any
+}
+
+abstract class B {
+    @Unique abstract var y: A
+}
+
+fun consume(@Unique a: Any) {}
+
+fun share(a: Any) {}
+
+// Consuming subproperties
+
+fun `consume shared subproperty`(z: B) {
+    consume(<!UNIQUENESS_VIOLATION!>z.y.x<!>)
+}
+
+fun `consume borrowed subproperty`(@Borrowed z: B) {
+    consume(<!UNIQUENESS_VIOLATION!>z.y.x<!>)
+}
+
+fun `consume unique subproperty`(@Unique z: B) {
+    consume(z.y.x)
+}
+
+fun `consume unique-borrowed subproperty`(@Borrowed @Unique z: B) {
+    consume(<!UNIQUENESS_VIOLATION!>z.y.x<!>)
+}
+
+fun `consume multiple unique subproperties`(@Unique z: B) {
+    consume(z.y.x)
+    consume(z.y.w)
+}
+
+// Consuming partially-inconsistent subproperties
+
+fun `consume partially moved`(@Unique z: B) {
+    consume(z.y)
+    consume(<!UNIQUENESS_VIOLATION!>z<!>)
+}
+
+fun `consume partially shared`(@Unique z: B) {
+    share(z.y)
+    consume(<!UNIQUENESS_VIOLATION!>z<!>)
+}
+
+// Consuming subproperty after assignment
+
+fun `consume unique parent after assigning subproperty to shared`(@Unique x: B, v: A) {
+    x.y = v
+    consume(<!UNIQUENESS_VIOLATION!>x<!>)
+}
+
+fun `consume unique parent after assigning subproperty to unique`(@Unique x: B, @Unique y: A) {
+    x.y = y
+    consume(x.y)
+}
