7f8a57bc2b5391c41386969b7677b5ca38e46e8e
Ramon Wick <60450391+The-Ray-Man@users.noreply.github.com>
2026-03-11 09:21:19 +0100

    Ramon/non unique names for functiontypeembedding (#58)
    This PR is a hot fix for the non-unique function type names. Instead of
    only considering the arguments for the function type, we also add the
    return type name
    
    ---------
    
    Co-authored-by: Ramon.Wick <ramon.wick@jetbrains.com>

--- formver.compiler-plugin/core/src/org/jetbrains/kotlin/formver/core/embeddings/types/FunctionTypeEmbedding.kt
+++ formver.compiler-plugin/core/src/org/jetbrains/kotlin/formver/core/embeddings/types/FunctionTypeEmbedding.kt
@@ -6,9 +6,7 @@
 package org.jetbrains.kotlin.formver.core.embeddings.types
 
 import org.jetbrains.kotlin.formver.core.domains.RuntimeTypeDomain
-import org.jetbrains.kotlin.formver.core.names.PretypeName
 import org.jetbrains.kotlin.formver.core.names.SetOfNames
-import org.jetbrains.kotlin.formver.viper.mangled
 
 data class FunctionTypeEmbedding(
     val dispatchReceiverType: TypeEmbedding?,
@@ -19,7 +17,7 @@ data class FunctionTypeEmbedding(
 ) : PretypeEmbedding {
     override val runtimeType = RuntimeTypeDomain.functionType()
 
-    override val name: SetOfNames = SetOfNames(formalArgTypes.map { it -> it.name })
+    override val name: SetOfNames = SetOfNames(formalArgTypes.map { it -> it.name } + listOf(returnType.name))
 
     /**
      * The flattened structure of the callable parameters: in case the callable has a receiver
