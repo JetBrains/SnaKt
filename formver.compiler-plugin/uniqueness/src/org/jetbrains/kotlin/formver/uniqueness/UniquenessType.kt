b4e4e30152c1791d3a41b1fbe34911eb490f0f97
marco <paganma@usi.ch>
2026-03-12 16:35:45 +0100

    Add Cone attribute for tracking uniqueness

--- formver.compiler-plugin/uniqueness/src/org/jetbrains/kotlin/formver/uniqueness/UniquenessType.kt
+++ formver.compiler-plugin/uniqueness/src/org/jetbrains/kotlin/formver/uniqueness/UniquenessType.kt
@@ -5,6 +5,9 @@
 
 package org.jetbrains.kotlin.formver.uniqueness
 
+import org.jetbrains.kotlin.fir.types.ConeAttribute
+import kotlin.reflect.KClass
+
 enum class UniqueLevel {
     Unique,
     Shared,
@@ -55,5 +58,35 @@ sealed interface UniquenessType {
                 }
             }
         }
+    }
+}
+
+class UniquenessConeAttribute(val type : UniquenessType) : ConeAttribute<UniquenessConeAttribute>() {
+    override val key: KClass<UniquenessConeAttribute>
+        get() = UniquenessConeAttribute::class
+
+    override val keepInInferredDeclarationType: Boolean
+        get() = true
+
+    override val implementsEquality: Boolean
+        get() = true
+
+    override fun union(other: UniquenessConeAttribute?): UniquenessConeAttribute? {
+        return UniquenessConeAttribute(type.join(other?.type ?: return null))
+    }
+
+    override fun intersect(other: UniquenessConeAttribute?): UniquenessConeAttribute? {
+        return UniquenessConeAttribute(type.join(other?.type ?: return null))
+    }
+
+    override fun add(other: UniquenessConeAttribute?): UniquenessConeAttribute? {
+        return UniquenessConeAttribute(type.join(other?.type ?: return null))
+    }
+
+    override fun isSubtypeOf(other: UniquenessConeAttribute?): Boolean
+            = true
+
+    override fun toString(): String {
+        return "@(${type})"
     }
 }
