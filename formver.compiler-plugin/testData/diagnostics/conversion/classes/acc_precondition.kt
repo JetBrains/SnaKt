109f8e0195d185ff9464ff930ab65403c1782f8d
TheDeveloper101 <saidivvela2005@gmail.com>
2026-03-17 15:25:29 -0400

    getting a NotImplemented error for some reason, need to do more debugging. fixed test

--- formver.compiler-plugin/testData/diagnostics/conversion/classes/acc_precondition.kt
+++ formver.compiler-plugin/testData/diagnostics/conversion/classes/acc_precondition.kt
@@ -1,8 +1,10 @@
+// RENDER_PREDICATES
 // NEVER_VALIDATE
 
+import org.jetbrains.kotlin.formver.plugin.Manual
 
-class X(var a: Any)
-fun <!VIPER_TEXT!>f<!>(@Manual var x: X) {
+class X(@property:Manual var a: Any)
+fun <!VIPER_TEXT!>test_acc_precondition<!>(var x: X) {
     preconditions { acc(x.a, write) }
     x.a = 123
 }
