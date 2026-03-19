53bfbab19385816e8fffcd651bdb2a0e841485d9
marco <paganma@usi.ch>
2026-03-19 18:04:30 +0100

    Register LocalityPropertyChecker, LocalityVariableAssignmentChecker, LocalityFunctionCallChecker in PluginAdditionalCheckers

--- formver.compiler-plugin/plugin/src/org/jetbrains/kotlin/formver/plugin/compiler/PluginAdditionalCheckers.kt
+++ formver.compiler-plugin/plugin/src/org/jetbrains/kotlin/formver/plugin/compiler/PluginAdditionalCheckers.kt
@@ -7,9 +7,16 @@ package org.jetbrains.kotlin.formver.plugin.compiler
 
 import org.jetbrains.kotlin.fir.FirSession
 import org.jetbrains.kotlin.fir.analysis.checkers.declaration.DeclarationCheckers
+import org.jetbrains.kotlin.fir.analysis.checkers.declaration.FirPropertyChecker
 import org.jetbrains.kotlin.fir.analysis.checkers.declaration.FirSimpleFunctionChecker
+import org.jetbrains.kotlin.fir.analysis.checkers.expression.ExpressionCheckers
+import org.jetbrains.kotlin.fir.analysis.checkers.expression.FirFunctionCallChecker
+import org.jetbrains.kotlin.fir.analysis.checkers.expression.FirVariableAssignmentChecker
 import org.jetbrains.kotlin.fir.analysis.extensions.FirAdditionalCheckersExtension
 import org.jetbrains.kotlin.formver.common.PluginConfiguration
+import org.jetbrains.kotlin.formver.plugin.compiler.locality.LocalityFunctionCallChecker
+import org.jetbrains.kotlin.formver.plugin.compiler.locality.LocalityPropertyChecker
+import org.jetbrains.kotlin.formver.plugin.compiler.locality.LocalityVariableAssignmentChecker
 
 class PluginAdditionalCheckers(session: FirSession, config: PluginConfiguration) :
     FirAdditionalCheckersExtension(session) {
@@ -22,6 +29,16 @@ class PluginAdditionalCheckers(session: FirSession, config: PluginConfiguration)
     override val declarationCheckers: DeclarationCheckers = object : DeclarationCheckers() {
         override val simpleFunctionCheckers: Set<FirSimpleFunctionChecker>
             get() = setOf(ViperPoweredDeclarationChecker(session, config), UniquenessDeclarationChecker(session, config))
+
+        override val propertyCheckers: Set<FirPropertyChecker>
+            get() = setOf(LocalityPropertyChecker(config))
+    }
+
+    override val expressionCheckers: ExpressionCheckers = object : ExpressionCheckers() {
+        override val variableAssignmentCheckers: Set<FirVariableAssignmentChecker>
+            get() = setOf(LocalityVariableAssignmentChecker(config))
+
+        override val functionCallCheckers: Set<FirFunctionCallChecker>
+            get() = setOf(LocalityFunctionCallChecker(config))
     }
 }
-
