79509c5828502c77d5b1381efac3953f15ac7003
Ramon.Wick <ramon.wick@jetbrains.com>
2026-03-19 13:46:49 +0100

    Refine node rendering to conditionally display relevant data.

--- formver.compiler-plugin/uniqueness/src/org/jetbrains/kotlin/formver/uniqueness/utils/CFGRenderer.kt
+++ formver.compiler-plugin/uniqueness/src/org/jetbrains/kotlin/formver/uniqueness/utils/CFGRenderer.kt
@@ -1,23 +1,47 @@
 package org.jetbrains.kotlin.formver.uniqueness.utils
 
-import org.jetbrains.kotlin.fir.resolve.dfa.cfg.CFGNode
-import org.jetbrains.kotlin.fir.resolve.dfa.cfg.ControlFlowGraph
-import org.jetbrains.kotlin.fir.resolve.dfa.cfg.ControlFlowGraphRenderOptions
-import org.jetbrains.kotlin.fir.resolve.dfa.cfg.renderTo
+import org.jetbrains.kotlin.fir.resolve.dfa.cfg.*
 import org.jetbrains.kotlin.formver.uniqueness.FlowFacts
 import org.jetbrains.kotlin.formver.uniqueness.UniquenessTrie
 
 
+/**
+ * This method creates the uniqueness-related content for each CFG Node. To not overload the graph with data, not everything is displayed.
+ * In these cases some flow information is added:
+ * - The flow changed when executing the node: In and Out flow are displayed
+ * - On the border of the function the flow is displayed
+ * - On nodes that join multiple flows the flow is displayed
+ */
 fun render(node: CFGNode<*>, facts: FlowFacts<UniquenessTrie>): String {
     val flowBefore = facts.flowBefore(node)
     val flowAfter = facts.flowAfter(node)
-    val builder = StringBuilder()
-
-    builder.appendLine("Before:")
-    builder.appendLine(flowBefore.render())
-    builder.appendLine("After:")
-    builder.appendLine(flowAfter.render())
-    return builder.toString()
+    return buildString {
+        when {
+            // flows differ
+            flowBefore != flowAfter -> {
+                appendLine("Before:")
+                appendLine(flowBefore.render())
+                appendLine()
+                appendLine("After:")
+                appendLine(flowAfter.render())
+            }
+            // function boundary start
+            node is FunctionEnterNode -> {
+                appendLine("Initial:")
+                appendLine(flowBefore.render())
+            }
+            // function boundary end
+            node is FunctionExitNode -> {
+                appendLine("Final:")
+                appendLine(flowAfter.render())
+            }
+            // joining node
+            node.previousNodes.count() > 1 -> {
+                appendLine("Merge:")
+                appendLine(flowAfter.render())
+            }
+        }
+    }
 }
 
 @Suppress("unused") // Can be used from the debugger
