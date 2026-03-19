27e36bdd17cbbb9e01de98622732e414b6330b81
marco <paganma@usi.ch>
2026-03-19 18:00:47 +0100

    Add LocalityVariableAssignmentChecker for checking locality in assignments

---
+++ formver.compiler-plugin/plugin/src/org/jetbrains/kotlin/formver/plugin/compiler/locality/LocalityVariableAssignmentChecker.kt
@@ -0,0 +1,32 @@
+package org.jetbrains.kotlin.formver.plugin.compiler.locality
+
+import org.jetbrains.kotlin.diagnostics.DiagnosticReporter
+import org.jetbrains.kotlin.diagnostics.reportOn
+import org.jetbrains.kotlin.fir.analysis.checkers.MppCheckerKind
+import org.jetbrains.kotlin.fir.analysis.checkers.context.CheckerContext
+import org.jetbrains.kotlin.fir.analysis.checkers.expression.FirVariableAssignmentChecker
+import org.jetbrains.kotlin.fir.expressions.FirVariableAssignment
+import org.jetbrains.kotlin.fir.types.resolvedType
+import org.jetbrains.kotlin.formver.common.PluginConfiguration
+import org.jetbrains.kotlin.formver.plugin.compiler.PluginErrors.LOCALITY_VIOLATION
+
+class LocalityVariableAssignmentChecker(
+    private val config : PluginConfiguration
+) : FirVariableAssignmentChecker(MppCheckerKind.Common) {
+    context(context: CheckerContext, reporter: DiagnosticReporter)
+    override fun check(expression: FirVariableAssignment) {
+        if (!config.checkLocality) return
+
+        val leftLocality = expression.lValue.resolvedType.localAttribute
+        val rightLocality = expression.rValue.resolvedType.localAttribute
+
+        if (leftLocality.accepts(rightLocality)) return
+
+        reporter.reportOn(
+            expression.rValue.source,
+            LOCALITY_VIOLATION,
+            "Assignment locality mismatch: expected '${leftLocality.render()}', " +
+                    "actual '${rightLocality.render()}'."
+        )
+    }
+}
