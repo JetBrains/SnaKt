22d9e351a9239362aa1af45c0e2fe7ae99f57106
marco <paganma@usi.ch>
2026-03-19 18:02:32 +0100

    Add LocalityFunctionCallChecker for checking locality in function calls

---
+++ formver.compiler-plugin/plugin/src/org/jetbrains/kotlin/formver/plugin/compiler/locality/LocalityFunctionCallChecker.kt
@@ -0,0 +1,44 @@
+package org.jetbrains.kotlin.formver.plugin.compiler.locality
+
+import org.jetbrains.kotlin.diagnostics.DiagnosticReporter
+import org.jetbrains.kotlin.diagnostics.reportOn
+import org.jetbrains.kotlin.fir.analysis.checkers.MppCheckerKind
+import org.jetbrains.kotlin.fir.analysis.checkers.context.CheckerContext
+import org.jetbrains.kotlin.fir.analysis.checkers.expression.FirFunctionCallChecker
+import org.jetbrains.kotlin.fir.expressions.FirFunctionCall
+import org.jetbrains.kotlin.fir.expressions.arguments
+import org.jetbrains.kotlin.fir.expressions.toResolvedCallableSymbol
+import org.jetbrains.kotlin.fir.resolve.defaultType
+import org.jetbrains.kotlin.fir.symbols.impl.FirFunctionSymbol
+import org.jetbrains.kotlin.fir.types.resolvedType
+import org.jetbrains.kotlin.formver.common.PluginConfiguration
+import org.jetbrains.kotlin.formver.plugin.compiler.PluginErrors.LOCALITY_VIOLATION
+
+class LocalityFunctionCallChecker(
+    private val config : PluginConfiguration
+) : FirFunctionCallChecker(MppCheckerKind.Common) {
+    @OptIn(SymbolInternals::class)
+    context(context: CheckerContext, reporter: DiagnosticReporter)
+    override fun check(expression: FirFunctionCall) {
+        if (!config.checkLocality) return
+
+        val callableSymbol = expression.toResolvedCallableSymbol() as? FirFunctionSymbol<*>
+            ?: throw IllegalStateException("Unable to resolve ${expression}")
+        val parameters = callableSymbol.valueParameterSymbols
+        val arguments = expression.arguments
+
+        for ((parameter, argument) in parameters.zip(arguments)) {
+            val parameterLocality = parameter.defaultType.localAttribute
+            val argumentLocality = argument.resolvedType.localAttribute
+
+            if (parameterLocality.accepts(argumentLocality)) continue
+
+            reporter.reportOn(
+                argument.source,
+                LOCALITY_VIOLATION,
+                "Argument uniqueness mismatch: expected '${parameterLocality.render()}', " +
+                        "actual '${argumentLocality.render()}'."
+            )
+        }
+    }
+}
