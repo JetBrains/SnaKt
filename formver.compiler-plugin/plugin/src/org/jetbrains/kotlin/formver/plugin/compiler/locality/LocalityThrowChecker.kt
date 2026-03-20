56664bcc060c07dd49319f69876eba20db91804a
marco <paganma@usi.ch>
2026-03-20 10:23:04 +0100

    Add LocalityThrowChecker for checking locality in throw expressions

--- formver.compiler-plugin/plugin/src/org/jetbrains/kotlin/formver/plugin/compiler/locality/LocalityThrowChecker.kt
+++ formver.compiler-plugin/plugin/src/org/jetbrains/kotlin/formver/plugin/compiler/locality/LocalityThrowChecker.kt
@@ -4,29 +4,28 @@ import org.jetbrains.kotlin.diagnostics.DiagnosticReporter
 import org.jetbrains.kotlin.diagnostics.reportOn
 import org.jetbrains.kotlin.fir.analysis.checkers.MppCheckerKind
 import org.jetbrains.kotlin.fir.analysis.checkers.context.CheckerContext
-import org.jetbrains.kotlin.fir.analysis.checkers.expression.FirReturnExpressionChecker
-import org.jetbrains.kotlin.fir.expressions.FirReturnExpression
-import org.jetbrains.kotlin.fir.types.coneType
+import org.jetbrains.kotlin.fir.analysis.checkers.expression.FirThrowExpressionChecker
+import org.jetbrains.kotlin.fir.expressions.FirThrowExpression
 import org.jetbrains.kotlin.fir.types.resolvedType
 import org.jetbrains.kotlin.formver.common.PluginConfiguration
 import org.jetbrains.kotlin.formver.plugin.compiler.PluginErrors.LOCALITY_VIOLATION
 
 class LocalityThrowChecker(
     private val config : PluginConfiguration
-) : FirReturnExpressionChecker(MppCheckerKind.Common) {
+) : FirThrowExpressionChecker(MppCheckerKind.Common) {
     context(context: CheckerContext, reporter: DiagnosticReporter)
-    override fun check(expression: FirReturnExpression) {
+    override fun check(expression: FirThrowExpression) {
         if (!config.checkLocality) return
 
-        val leftLocality = expression.target.labeledElement.returnTypeRef.coneType.localAttribute
-        val rightLocality = expression.result.resolvedType.localAttribute
+        val leftLocality: ConeLocalAttribute? = null
+        val rightLocality = expression.exception.resolvedType.localAttribute
 
         if (leftLocality.accepts(rightLocality)) return
 
         reporter.reportOn(
-            expression.source,
-            UNIQUENESS_VIOLATION,
-            "Return locality mismatch: expected '${leftLocality.render()}', " +
+            expression.exception.source,
+            LOCALITY_VIOLATION,
+            "Throw locality mismatch: expected '${leftLocality.render()}', " +
                     "actual '${rightLocality.render()}'."
         )
     }
