7d9669bdefaca007e78948e43580fba878effe23
marco <paganma@usi.ch>
2026-03-22 23:02:33 +0100

    Cache ConeLocalAttributeExtractor
    Rename ConeLocalAttributeExtractor.extract to extractLocalAttribute

--- formver.compiler-plugin/plugin/src/org/jetbrains/kotlin/formver/plugin/compiler/locality/ConeLocalAttributeExtractor.kt
+++ formver.compiler-plugin/plugin/src/org/jetbrains/kotlin/formver/plugin/compiler/locality/ConeLocalAttributeExtractor.kt
@@ -6,17 +6,21 @@ import org.jetbrains.kotlin.fir.expressions.FirQualifiedAccessExpression
 import org.jetbrains.kotlin.fir.types.resolvedType
 import org.jetbrains.kotlin.fir.visitors.FirVisitor
 import org.jetbrains.kotlin.formver.plugin.compiler.fir.FirTailsExtractor
+import java.util.WeakHashMap
 
 class ConeLocalAttributeExtractor(
+    val cache: WeakHashMap<FirElement, ConeLocalAttribute?>,
     val tailsExtractor: FirTailsExtractor
 ) : FirVisitor<ConeLocalAttribute?, Unit>() {
     private fun FirExpression.visit(): ConeLocalAttribute? {
         return accept(this@ConeLocalAttributeExtractor, Unit)
     }
 
-    fun extract(element: FirElement): ConeLocalAttribute? {
-        return tailsExtractor.extract(element).fold(null) { result, next ->
-            result?.union(next.visit())
+    fun extractLocalAttribute(element: FirElement): ConeLocalAttribute? {
+        return cache.computeIfAbsent(element) {
+            tailsExtractor.extractTails(element).fold(null) { result, next ->
+                result?.union(next.visit())
+            }
         }
     }
 
