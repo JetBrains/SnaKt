ea1cec24950b3bb9a9eea335cf3edf48b3d0b5cf
marco <paganma@usi.ch>
2026-03-22 22:49:11 +0100

    Add ConeLocalAttributeProvider to cache ConeLocalAttributeExtractor

---
+++ formver.compiler-plugin/plugin/src/org/jetbrains/kotlin/formver/plugin/compiler/locality/ConeLocalAttributeProvider.kt
@@ -0,0 +1,30 @@
+package org.jetbrains.kotlin.formver.plugin.compiler.locality
+
+import org.jetbrains.kotlin.fir.FirElement
+import org.jetbrains.kotlin.fir.FirSession
+import org.jetbrains.kotlin.fir.FirSession.Companion.sessionComponentAccessor
+import org.jetbrains.kotlin.fir.extensions.FirExtensionSessionComponent
+import org.jetbrains.kotlin.formver.plugin.compiler.fir.firTails
+import java.util.WeakHashMap
+
+class ConeLocalAttributeProvider(
+    session: FirSession,
+    val localAttributeExtractor: ConeLocalAttributeExtractor
+) : FirExtensionSessionComponent(session) {
+    companion object {
+        fun getFactory(): Factory = Factory { session ->
+            ConeLocalAttributeProvider(
+                session,
+                ConeLocalAttributeExtractor(
+                    WeakHashMap(),
+                    session.firTails.tailsExtractor
+                )
+            )
+        }
+    }
+    
+    operator fun get(element: FirElement): ConeLocalAttribute? =
+        localAttributeExtractor.extractLocalAttribute(element)
+}
+
+val FirSession.coneLocalAttributes: ConeLocalAttributeProvider by sessionComponentAccessor()
