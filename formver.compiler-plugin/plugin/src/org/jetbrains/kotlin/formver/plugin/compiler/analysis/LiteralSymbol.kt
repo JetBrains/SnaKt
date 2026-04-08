419eb68170cdd79ffbbc8baf49310a0d29bbb2cf
marco <paganma@usi.ch>
2026-04-08 09:25:31 +0200

    fixup! Add immutable implementation of PathTrie

--- formver.compiler-plugin/plugin/src/org/jetbrains/kotlin/formver/plugin/compiler/analysis/LiteralSymbol.kt
+++ formver.compiler-plugin/plugin/src/org/jetbrains/kotlin/formver/plugin/compiler/analysis/LiteralSymbol.kt
@@ -3,5 +3,4 @@ package org.jetbrains.kotlin.formver.plugin.compiler.analysis
 import org.jetbrains.kotlin.fir.declarations.FirDeclaration
 import org.jetbrains.kotlin.fir.symbols.FirBasedSymbol
 
-object LiteralSymbol : FirBasedSymbol<FirDeclaration>() {
-}
+object LiteralSymbol : FirBasedSymbol<FirDeclaration>()
