3ab97211bc0192ceb7db31d46105f03ca2b452b2
marco <paganma@usi.ch>
2026-04-09 17:07:11 +0200

    fixup! Check locality with respect to declaration

--- formver.compiler-plugin/testData/diagnostics/locality_checker/assign_local.kt
+++ formver.compiler-plugin/testData/diagnostics/locality_checker/assign_local.kt
@@ -41,3 +41,128 @@ fun `assign local to local in lambda`(x: @Borrowed Any) {
     }
 }
 
+fun `assign local if-expression to local`(x: @Borrowed Any) {
+    var z: @Borrowed Any = if (false) { x } else { Any() }
+}
+
+fun `assign local if-expression to global`(x: @Borrowed Any) {
+    var z: Any = <!LOCALITY_VIOLATION!>if (false) { x } else { Any() }<!>
+}
+
+fun `assign local if-expression to local in lambda`(x: @Borrowed Any) {
+    { y: Any ->
+        var z: @Borrowed Any = <!LOCALITY_VIOLATION!>if (false) { x } else { Any() }<!>
+    }
+}
+
+fun `assign local if-expression to local in loop`(x: @Borrowed Any) {
+    var z: @Borrowed Any = Any()
+
+    while (true) {
+        z = if (false) { x } else { Any() }
+    }
+}
+
+fun `assign local when-expression to local`(x: @Borrowed Any) {
+    var z: @Borrowed Any = when {
+        false -> x
+        else -> Any()
+    }
+}
+
+fun `assign local when-expression to global`(x: @Borrowed Any) {
+    var z: Any = <!LOCALITY_VIOLATION!>when {
+        false -> x
+        else -> Any()
+    }<!>
+}
+
+fun `assign local when-expression to global in loop`(x: @Borrowed Any) {
+    var z: Any = Any()
+
+    while (true) {
+        z = <!LOCALITY_VIOLATION!>when {
+            false -> x
+            else -> Any()
+        }<!>
+    }
+}
+
+fun `assign local try-expression to global`(x: @Borrowed Any) {
+    var z: Any = <!LOCALITY_VIOLATION!>try {
+        x
+    } catch (_: Throwable) {
+        Any()
+    }<!>
+}
+
+fun `assign local try-expression to global in loop`(x: @Borrowed Any) {
+    var z: Any = Any()
+
+    while (true) {
+        z = <!LOCALITY_VIOLATION!>try {
+            x
+        } catch (_: Throwable) {
+            Any()
+        }<!>
+    }
+}
+
+fun `assign local cast-expression to global`(x: @Borrowed Any) {
+    var z: String = <!LOCALITY_VIOLATION!>x as String<!>
+}
+
+fun `assign local not-null-expression to global`(x: @Borrowed Any?) {
+    var z: Any = <!LOCALITY_VIOLATION!>x!!<!>
+}
+
+fun `assign local nested control-flow to global in loop`(x: @Borrowed Any) {
+    var z: Any = Any()
+
+    while (true) {
+        z = <!LOCALITY_VIOLATION!>if (false) {
+            when {
+                false -> x
+                else -> Any()
+            }
+        } else {
+            Any()
+        }<!>
+
+        z = <!LOCALITY_VIOLATION!>try {
+            if (false) { x } else { Any() }
+        } catch (_: Throwable) {
+            Any()
+        }<!>
+    }
+}
+
+fun `assign local nested control-flow to local in loop`(x: @Borrowed Any) {
+    var z: @Borrowed Any = Any()
+
+    while (true) {
+        z = when {
+            false -> if (false) { x } else { Any() }
+            else -> x
+        }
+    }
+}
+
+fun `assign local nested control-flow to global in lambda loop`(x: @Borrowed Any) {
+    {
+        var z: Any = Any()
+
+        while (true) {
+            z = <!LOCALITY_VIOLATION!>when {
+                false -> try { x } catch (_: Throwable) { Any() }
+                else -> Any()
+            }<!>
+
+            z = <!LOCALITY_VIOLATION!>if (false) {
+                x
+            } else {
+                try { Any() } catch (_: Throwable) { x }
+            }<!>
+        }
+    }
+}
