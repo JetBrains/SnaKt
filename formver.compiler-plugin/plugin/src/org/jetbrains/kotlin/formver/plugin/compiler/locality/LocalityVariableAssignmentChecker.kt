6655315f422ed9c23d29b6e9933f068ab3a86448
marco <paganma@usi.ch>
2026-04-10 18:09:42 +0200

    fixup! 93112cdec36219a093cd31845cfc8161821a5a50

--- formver.compiler-plugin/plugin/src/org/jetbrains/kotlin/formver/plugin/compiler/locality/LocalityVariableAssignmentChecker.kt
+++ formver.compiler-plugin/plugin/src/org/jetbrains/kotlin/formver/plugin/compiler/locality/LocalityVariableAssignmentChecker.kt
@@ -16,8 +16,8 @@ class LocalityVariableAssignmentChecker(
     override fun check(expression: FirVariableAssignment) {
         if (!config.checkLocality) return
 
-        val leftLocality = expression.lValue.localityAttribute
-        val rightLocality = expression.rValue.localityAttribute
+        val leftLocality = expression.lValue.resolvedLocality
+        val rightLocality = expression.rValue.resolvedLocality
 
         if (leftLocality.accepts(rightLocality)) return
 
