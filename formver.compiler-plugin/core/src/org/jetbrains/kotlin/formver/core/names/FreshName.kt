8cd43a6bc7fbb71d79fa8040c9d23d58eb55eebc
Ramon.Wick <ramon.wick@jetbrains.com>
2026-04-10 15:06:09 +0200

    Removed name resolver specific functions from the Symbolic Names

--- formver.compiler-plugin/core/src/org/jetbrains/kotlin/formver/core/names/FreshName.kt
+++ formver.compiler-plugin/core/src/org/jetbrains/kotlin/formver/core/names/FreshName.kt
@@ -6,11 +6,8 @@
 package org.jetbrains.kotlin.formver.core.names
 
 import org.jetbrains.kotlin.formver.core.embeddings.types.TypeEmbedding
-import org.jetbrains.kotlin.formver.viper.NameResolver
 import org.jetbrains.kotlin.formver.viper.NameType
-import org.jetbrains.kotlin.formver.viper.NameType.Label
 import org.jetbrains.kotlin.formver.viper.SymbolicName
-import org.jetbrains.kotlin.formver.viper.mangled
 
 /* This file contains mangled names for constructs introduced during the conversion to Viper.
  *
@@ -20,46 +17,37 @@ import org.jetbrains.kotlin.formver.viper.mangled
 
 sealed interface FreshName : SymbolicName
 
-abstract class TypedFreshName(override val mangledType: NameType, val baseName: String) : FreshName {
-    context(nameResolver: NameResolver)
-    override val mangledBaseName: String
-        get() = baseName
-}
+abstract class TypedFreshName(override val mangledType: NameType, val baseName: String) : FreshName
 
 /**
  * Representation for names not present in the original source,
  * e.g. storage for the result of subexpressions.
  */
-data class AnonymousName(val n: Int) : TypedFreshName(NameType.Variable, "anon$$n")
+data class AnonymousName(val n: Int) : TypedFreshName(NameType.Base.Variable, "anon$$n")
 
-data class AnonymousBuiltinName(val n: Int) : TypedFreshName(NameType.Variable, $$"anon$builtin$$$n")
+data class AnonymousBuiltinName(val n: Int) : TypedFreshName(NameType.Base.Variable, $$"anon$builtin$$$n")
 
 /**
  * Name for return variable that should *only* be used in signatures of methods without a body.
  */
-data object PlaceholderReturnVariableName : TypedFreshName(NameType.Variable, "ret")
+data object PlaceholderReturnVariableName : TypedFreshName(NameType.Base.Variable, "ret")
 
-data class ReturnVariableName(val n: Int) : TypedFreshName(NameType.Variable, "ret$$n")
+data class ReturnVariableName(val n: Int) : TypedFreshName(NameType.Base.Variable, "ret$$n")
 
 /**
  * Name for return variable that should *only* be used in signatures of pure functions
  * This variable will be translated into the special result variable in Viper
  */
-data object FunctionResultVariableName : TypedFreshName(NameType.Variable, "result")
+data object FunctionResultVariableName : TypedFreshName(NameType.Base.Variable, "result")
 
-data object DispatchReceiverName : TypedFreshName(NameType.Variable, $$"this$dispatch")
+data object DispatchReceiverName : TypedFreshName(NameType.Base.Variable, $$"this$dispatch")
 
-data object ExtensionReceiverName : TypedFreshName(NameType.Variable, $$"this$extension")
+data object ExtensionReceiverName : TypedFreshName(NameType.Base.Variable, $$"this$extension")
 
-data class SpecialFieldName(val name: String) : TypedFreshName(NameType.Property, name)
+data class SpecialFieldName(val name: String) : TypedFreshName(NameType.Member.Property, name)
 
 
-sealed class NumberedLabelName(name: String, val originalN: Int) : TypedFreshName(Label, name) {
-
-    context(nameResolver: NameResolver)
-    override val mangledBaseName: String
-        get() = "$baseName$$originalN"
-}
+sealed class NumberedLabelName(name: String, val originalN: Int) : TypedFreshName(NameType.Base.Label, name) {}
 
 data class ReturnLabelName(val scopeDepth: Int) : NumberedLabelName("ret", scopeDepth)
 data class BreakLabelName(val n: Int) : NumberedLabelName("break", n)
@@ -68,27 +56,20 @@ data class CatchLabelName(val n: Int) : NumberedLabelName("catch", n)
 data class TryExitLabelName(val n: Int) : NumberedLabelName("tryExit", n)
 
 
-data class DomainAssociatedFuncName(val name: String) : TypedFreshName(NameType.DomainFunction, name)
+data class DomainAssociatedFuncName(val name: String) : TypedFreshName(NameType.Base.DomainFunction, name)
 
-data class PlaceholderArgumentName(val n: Int) : TypedFreshName(NameType.Variable, "arg$$n")
+data class PlaceholderArgumentName(val n: Int) : TypedFreshName(NameType.Base.Variable, "arg$$n")
 
-data class DomainFuncParameterName(val name: String) : TypedFreshName(NameType.Variable, name)
+data class DomainFuncParameterName(val name: String) : TypedFreshName(NameType.Base.Variable, name)
 
 data class SsaVariableName(val ssaIndex: Int, val baseName: SymbolicName) : FreshName {
     override val mangledType: NameType
-        get() = NameType.Variable
-
-    context(nameResolver: NameResolver)
-    override val mangledBaseName: String
-        get() = "${baseName.mangled}$$ssaIndex"
+        get() = NameType.Base.Variable
 }
 
-data class PredicateName(val name: String) : TypedFreshName(NameType.Predicate, name)
+data class PredicateName(val name: String) : TypedFreshName(NameType.Base.Predicate, name)
 
 data class HavocName(val type: TypeEmbedding) : SymbolicName {
-    context(nameResolver: NameResolver)
-    override val mangledBaseName: String
-        get() = type.name.mangled
     override val mangledType: NameType
-        get() = NameType.Havoc
+        get() = NameType.Base.Havoc
 }
--- formver.compiler-plugin/core/src/org/jetbrains/kotlin/formver/core/names/KotlinName.kt
+++ formver.compiler-plugin/core/src/org/jetbrains/kotlin/formver/core/names/KotlinName.kt
@@ -9,7 +9,8 @@ import org.jetbrains.kotlin.formver.core.embeddings.types.FunctionTypeEmbedding
 import org.jetbrains.kotlin.formver.core.embeddings.types.PretypeEmbedding
 import org.jetbrains.kotlin.formver.core.embeddings.types.TypeEmbedding
 import org.jetbrains.kotlin.formver.core.embeddings.types.asTypeEmbedding
-import org.jetbrains.kotlin.formver.viper.*
+import org.jetbrains.kotlin.formver.viper.NameType
+import org.jetbrains.kotlin.formver.viper.SymbolicName
 import org.jetbrains.kotlin.name.FqName
 import org.jetbrains.kotlin.name.Name
 
@@ -22,15 +23,9 @@ import org.jetbrains.kotlin.name.Name
 sealed interface KotlinName : SymbolicName
 
 data class SimpleKotlinName(val name: Name) : KotlinName {
-    context(nameResolver: NameResolver)
-    override val mangledBaseName: String
-        get() = name.asStringStripSpecialMarkers()
 }
 
 abstract class TypedKotlinName(override val mangledType: NameType, open val name: Name) : KotlinName {
-    context(nameResolver: NameResolver)
-    override val mangledBaseName: String
-        get() = name.asStringStripSpecialMarkers()
 }
 
 abstract class TypedKotlinNameWithType(
@@ -39,89 +34,57 @@ abstract class TypedKotlinNameWithType(
     val type: TypeEmbedding
 ) :
     KotlinName {
-    context(nameResolver: NameResolver)
-    override val mangledBaseName: String
-        get() = "${name.asStringStripSpecialMarkers()}$SEPARATOR${type.name.mangled}"
 }
 
 data class FunctionKotlinName(override val name: Name, val functionType: FunctionTypeEmbedding) :
     TypedKotlinNameWithType(
-        NameType.Function, name,
+        NameType.Base.Function, name,
         functionType.asTypeEmbedding()
     )
 
 /**
  * This name will never occur in the viper output, but rather is used to lookup properties.
  */
-data class PropertyKotlinName(override val name: Name) : TypedKotlinName(NameType.Property, name)
-data class BackingFieldKotlinName(override val name: Name) : TypedKotlinName(NameType.BackingField, name)
-data class GetterKotlinName(override val name: Name) : TypedKotlinName(NameType.Getter, name)
-data class SetterKotlinName(override val name: Name) : TypedKotlinName(NameType.Setter, name)
+data class PropertyKotlinName(override val name: Name) : TypedKotlinName(NameType.Member.Property, name)
+data class BackingFieldKotlinName(override val name: Name) : TypedKotlinName(NameType.Member.BackingField, name)
+data class GetterKotlinName(override val name: Name) : TypedKotlinName(NameType.Member.Getter, name)
+data class SetterKotlinName(override val name: Name) : TypedKotlinName(NameType.Member.Setter, name)
 data class ExtensionSetterKotlinName(override val name: Name, val functionType: FunctionTypeEmbedding) :
-    TypedKotlinNameWithType(NameType.ExtensionSetter, name, functionType.asTypeEmbedding())
+    TypedKotlinNameWithType(NameType.Member.ExtensionSetter, name, functionType.asTypeEmbedding())
 
 data class ExtensionGetterKotlinName(override val name: Name, val functionType: FunctionTypeEmbedding) :
-    TypedKotlinNameWithType(NameType.ExtensionGetter, name, functionType.asTypeEmbedding())
+    TypedKotlinNameWithType(NameType.Member.ExtensionGetter, name, functionType.asTypeEmbedding())
 
 data class ClassKotlinName(val name: FqName) : KotlinName {
     override val mangledType: NameType
-        get() = NameType.Type.Class
-
-    context(nameResolver: NameResolver)
-    override val mangledBaseName: String
-        get() = name.asViperString()
+        get() = NameType.TypeCategory.Class
 
     constructor(classSegments: List<String>) : this(FqName.fromSegments(classSegments))
 }
 
 data class ConstructorKotlinName(val type: FunctionTypeEmbedding) : KotlinName {
     override val mangledType: NameType
-        get() = NameType.Constructor
+        get() = NameType.Base.Constructor
 
-    context(nameResolver: NameResolver)
-    override val mangledBaseName: String
-        get() = type.name.mangledBaseName
 }
 
 sealed interface NameOfType : SymbolicName
 
 data class PretypeName(val name: String) : NameOfType {
 
-    context(nameResolver: NameResolver)
-    override val mangledBaseName: String
-        get() = name
 }
 
 data class ListOfNames<T : SymbolicName>(val names: List<T>) : SymbolicName {
     override val mangledType: NameType
-        get() = NameType.Type
+        get() = NameType.TypeCategory.GeneralType
 
-    context(nameResolver: NameResolver)
-    override val mangledBaseName: String
-        get() = names.joinToString(SEPARATOR) { it.mangled }
 }
 
-data class FunctionTypeName(val args: ListOfNames<NameOfType>, val returns: TypeName) : NameOfType {
-    context(nameResolver: NameResolver)
-    override val mangledBaseName: String
-        get() = buildString {
-            append(args.mangledBaseName)
-            if (isNotEmpty()) append(SEPARATOR)
-            append(returns.mangled)
-        }
-}
+data class FunctionTypeName(val args: ListOfNames<NameOfType>, val returns: TypeName) : NameOfType
 
 data class TypeName(val pretype: PretypeEmbedding, val nullable: Boolean) : NameOfType {
 
     override val mangledType: NameType
-        get() = NameType.Type
+        get() = NameType.TypeCategory.GeneralType
 
-    context(nameResolver: NameResolver)
-    override val mangledBaseName: String
-        get() = buildString {
-            if (nullable) append("N")
-            if (pretype is FunctionTypeEmbedding) append("F")
-            if (isNotEmpty()) append(SEPARATOR)
-            append(pretype.name.mangledBaseName)
-        }
 }
