6471a9bfd2cdaa43d77b452aa3b7cc849e7ed7d9
marco <paganma@usi.ch>
2026-04-10 11:48:15 +0200

    fixup! Test Locality checker

--- formver.compiler-plugin/testData/diagnostics/locality_checker/assign_property.kt
+++ formver.compiler-plugin/testData/diagnostics/locality_checker/assign_property.kt
@@ -18,8 +18,8 @@ fun `assign global safe-call expression to local`(x: A?) {
     @Borrowed var z: Any? = x?.x
 }
 
-fun `assign local safe-call expression to local`(x: @Borrowed A?) {
-    var z: @Borrowed Any? = x?.x
+fun `assign local safe-call expression to local`(@Borrowed x: A?) {
+    @Borrowed var z: Any? = x?.x
 }
 
 fun `assign local safe-call expression to global`(x: @Borrowed A?) {
