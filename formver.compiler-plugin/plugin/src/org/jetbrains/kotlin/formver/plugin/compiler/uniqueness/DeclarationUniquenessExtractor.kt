66bc10875467a98dd0a47f33139b86787f44e14a
marco <paganma@usi.ch>
2026-04-14 15:03:06 +0200

    Add DeclarationUniquenessExtractor

---
+++ formver.compiler-plugin/plugin/src/org/jetbrains/kotlin/formver/plugin/compiler/uniqueness/DeclarationUniquenessExtractor.kt
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2010-2026 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.formver.plugin.compiler.uniqueness
+
+import org.jetbrains.kotlin.fir.analysis.checkers.context.CheckerContext
+import org.jetbrains.kotlin.fir.declarations.FirDeclaration
+import org.jetbrains.kotlin.fir.types.classId
+import org.jetbrains.kotlin.fir.types.coneType
+import org.jetbrains.kotlin.formver.core.annotationId
+
+/**
+ * Extracts the uniqueness required by a declaration definition.
+ */
+class DeclarationUniquenessExtractor {
+    companion object {
+        val uniquenessAnnotationId = annotationId("Unique")
+
+        val FirDeclaration.hasUniquenessAnnotation: Boolean
+            get() = annotations.any { it.annotationTypeRef.coneType.classId == uniquenessAnnotationId }
+    }
+
+    fun extract(declaration: FirDeclaration): Uniqueness =
+        if (declaration.hasUniquenessAnnotation) {
+            Uniqueness.Unique
+        } else {
+            Uniqueness.Shared
+        }
+}
+
+/**
+ * Extracts the uniqueness required by [this] declaration definition.
+ */
+context(_: CheckerContext)
+val FirDeclaration.requiredUniqueness: Uniqueness
+    get() = DeclarationUniquenessExtractor().extract(this)
