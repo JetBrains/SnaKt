ae3e258609318eec7c6b94bf4f3d938d32c327e7
Ramon.Wick <ramon.wick@jetbrains.com>
2026-04-16 14:42:30 +0200

    use new test driver

--- formver.compiler-plugin/test-fixtures/org/jetbrains/kotlin/formver/plugin/GenerateTests.kt
+++ formver.compiler-plugin/test-fixtures/org/jetbrains/kotlin/formver/plugin/GenerateTests.kt
@@ -5,20 +5,22 @@
 
 package org.jetbrains.kotlin.formver.plugin
 
-import org.jetbrains.kotlin.formver.plugin.runners.AbstractFirLightTreeFormVerPluginNoVerificationDiagnosticsTest
-import org.jetbrains.kotlin.formver.plugin.runners.AbstractFirLightTreeFormVerPluginDiagnosticsTest
+import org.jetbrains.kotlin.formver.plugin.runners.AbstractPhasedDiagnosticTest
 import org.jetbrains.kotlin.generators.dsl.junit5.generateTestGroupSuiteWithJUnit5
 
 fun main() {
     generateTestGroupSuiteWithJUnit5 {
         testGroup(testDataRoot = "formver.compiler-plugin/testData", testsRoot = "formver.compiler-plugin/test-gen") {
-            testClass<AbstractFirLightTreeFormVerPluginDiagnosticsTest> {
+//            testClass<AbstractFirLightTreeFormVerPluginDiagnosticsTest> {
+//                model("diagnostics")
+//            }
+//            testClass<AbstractFirLightTreeFormVerPluginNoVerificationDiagnosticsTest> {
+//                model("diagnostics/verification")
+//                model("diagnostics/stdlib")
+//                model("diagnostics/expensive_verification")
+//            }
+            testClass<AbstractPhasedDiagnosticTest> {
                 model("diagnostics")
-            }
-            testClass<AbstractFirLightTreeFormVerPluginNoVerificationDiagnosticsTest> {
-                model("diagnostics/verification")
-                model("diagnostics/stdlib")
-                model("diagnostics/expensive_verification")
             }
         }
     }
