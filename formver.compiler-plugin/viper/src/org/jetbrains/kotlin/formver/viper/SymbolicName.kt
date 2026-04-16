2e2ec1b425fb6a99c1d8b5adfdca59569967a757
Ramon.Wick <ramon.wick@jetbrains.com>
2026-04-16 09:26:15 +0200

    Update NameType to Enum

--- formver.compiler-plugin/viper/src/org/jetbrains/kotlin/formver/viper/SymbolicName.kt
+++ formver.compiler-plugin/viper/src/org/jetbrains/kotlin/formver/viper/SymbolicName.kt
@@ -26,9 +26,11 @@ const val SEPARATOR = "$"
 interface SymbolicName : AnyName {
     val nameType: NameType?
         get() = null
+
     context(nameResolver: NameResolver)
     val mangledScope: String?
         get() = null
+
     context(nameResolver: NameResolver)
     val mangledBaseName: String
 }
@@ -38,30 +40,21 @@ val SymbolicName.mangled: String
     get() = nameResolver.lookup(this)
 
 
-
 /**
  * Collects all types of names we can have.
  */
-sealed class NameType(val name: String) : AnyName {
+sealed interface NameType : AnyName {
 
-    override fun toString(): String = name
-
-    object Property : NameType("p")
-    object BackingField : NameType("bf")
-    object Getter : NameType("g")
-    object Setter : NameType("s")
-    object ExtensionSetter : NameType("es")
-    object ExtensionGetter : NameType("eg")
-    object Type : NameType("t") {
-        object Class : NameType("c")
+    enum class Member : NameType {
+        Property, BackingField, Getter, Setter, ExtensionSetter, ExtensionGetter
     }
 
-    object Constructor : NameType("con")
-    object Function : NameType("f")
-    object Predicate : NameType("pred")
-    object Havoc : NameType("havoc")
-    object Label : NameType("lbl")
-    object Variable : NameType("v")
-    object Domain : NameType("d")
-    object DomainFunction : NameType("df")
+    enum class Base : NameType {
+        Constructor, Function, Predicate, Havoc, Variable, Domain, DomainFunction, Label
+    }
+
+    enum class TypeCategory : NameType {
+        Class, GeneralType
+    }
+
 }
