ab6da5fbda9b11604fdd90663526e391fd5a696d
Ramon.Wick <ramon.wick@jetbrains.com>
2026-04-17 15:50:12 +0200

    fixup! add fold+unfold ExpEmbedding

--- formver.compiler-plugin/core/src/org/jetbrains/kotlin/formver/core/embeddings/expression/Folding.kt
+++ formver.compiler-plugin/core/src/org/jetbrains/kotlin/formver/core/embeddings/expression/Folding.kt
@@ -5,22 +5,22 @@ import org.jetbrains.kotlin.formver.core.embeddings.types.TypeEmbedding
 import org.jetbrains.kotlin.formver.core.embeddings.types.buildType
 
 data class FoldEmbedding(
-    val predicate: PredicateAccessPermissions,
+    val path: PrimitiveFieldAccess,
 ) : ExpEmbedding {
     override val type: TypeEmbedding
         get() = buildType { boolean() }
 
     override fun <R> accept(v: ExpVisitor<R>): R = v.visitFoldEmbedding(this)
-    override fun children(): Sequence<ExpEmbedding> = sequenceOf(predicate)
+    override fun children(): Sequence<ExpEmbedding> = sequenceOf(path)
 }
 
 data class UnfoldEmbedding(
-    val predicate: PredicateAccessPermissions,
+    val path: PrimitiveFieldAccess,
 ) : ExpEmbedding {
     override val type: TypeEmbedding
         get() = buildType { boolean() }
 
     override fun <R> accept(v: ExpVisitor<R>): R = v.visitUnfoldEmbedding(this)
-    override fun children(): Sequence<ExpEmbedding> = sequenceOf(predicate)
+    override fun children(): Sequence<ExpEmbedding> = sequenceOf(path)
 }
 
