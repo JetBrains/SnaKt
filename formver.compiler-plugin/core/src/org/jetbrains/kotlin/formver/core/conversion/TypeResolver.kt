388d7e851f9317050ef7a1a13ea765e9742d2607
Ramon.Wick <ramon.wick@jetbrains.com>
2026-04-24 16:15:14 +0200

    fixup! Implement TypeResolver

--- formver.compiler-plugin/core/src/org/jetbrains/kotlin/formver/core/conversion/TypeResolver.kt
+++ formver.compiler-plugin/core/src/org/jetbrains/kotlin/formver/core/conversion/TypeResolver.kt
@@ -7,65 +7,57 @@ import org.jetbrains.kotlin.formver.viper.SymbolicName
 
 class TypeResolver {
 
-    private val classTypes = mutableMapOf<SymbolicName, ClassTypeEmbedding>()
+    private val classEmbedding = mutableMapOf<SymbolicName, ClassTypeEmbedding>()
 
-    private val interfaceTypes = mutableMapOf<SymbolicName, ClassTypeEmbedding>()
+    private val interfaceEmbedding = mutableMapOf<SymbolicName, ClassTypeEmbedding>()
 
-    private val types
-        get() = classTypes + interfaceTypes
+    private val embedding
+        get() = classEmbedding + interfaceEmbedding
 
-    private val superTypes = mutableMapOf<SymbolicName, Set<SymbolicName>>()
+    private val superTypes = mutableMapOf<SymbolicName, MutableSet<SymbolicName>>()
 
-    private val details = mutableMapOf<SymbolicName, ClassEmbeddingDetails>()
+    private val embeddingDetails = mutableMapOf<SymbolicName, ClassEmbeddingDetails>()
 
     private val classToFields = mutableMapOf<SymbolicName, Set<Pair<SymbolicName, FieldEmbedding>>>()
 
+    fun addSubtypeRelation(subtype: SymbolicName, supertype: SymbolicName) =
+        superTypes.getOrPut(subtype) {
+            mutableSetOf()
+        }.add(supertype)
 
-    /**
-     * Maps the name of a field to the name of a class it belongs to.
-     */
-    val fieldToClassMapping = mutableMapOf<SymbolicName, SymbolicName>()
-
-
-    fun extendsTypeHierarchy(subtype: SymbolicName, supertype: List<SymbolicName>) {
-        superTypes.merge(subtype, supertype.toSet()) { a, b -> a + b }
-    }
 
     fun addFieldToClass(klass: SymbolicName, fieldName: SymbolicName, fieldEmbedding: FieldEmbedding) {
         classToFields.merge(klass, setOf(Pair(fieldName, fieldEmbedding))) { a, b -> (a + b) }
     }
 
     fun register(name: SymbolicName, typeEmbedding: ClassTypeEmbedding, isInterface: Boolean) = when (isInterface) {
-        true -> interfaceTypes.putIfAbsent(name, typeEmbedding)
-        false -> classTypes.putIfAbsent(name, typeEmbedding)
-
+        true -> interfaceEmbedding.putIfAbsent(name, typeEmbedding)
+        false -> classEmbedding.putIfAbsent(name, typeEmbedding)
     }
 
-    fun lookupSuperTypes(name: SymbolicName) = superTypes.getOrDefault(name, emptySet()).mapNotNull { types[it] }
-    fun lookupClassType(name: SymbolicName) = types[name]
-    fun classTypes() = classTypes.values
-
-    fun finish(name: SymbolicName) {
-        require(!details.containsKey(name)) { "Class type details for $name have already been finalized" }
-        details[name] = details(name)
-    }
-
-    fun isRegistered(name: SymbolicName) = types.containsKey(name)
-
-    fun hasDetails(name: SymbolicName) = details.containsKey(name)
+    fun lookupSuperTypes(name: SymbolicName) = superTypes.getOrDefault(name, emptySet()).mapNotNull { embedding[it] }
+    fun lookupClassType(name: SymbolicName) = embedding[name]
+    fun classTypes() = classEmbedding.values
 
 
-    fun allDetails(): List<ClassEmbeddingDetails> = types.values.map { details(it.name) }
-    fun details(name: SymbolicName): ClassEmbeddingDetails = details.getOrPut(name)
+    fun embedding(name: SymbolicName) = embedding[name]
+    fun allEmbeddings() = embedding.values
+
+    fun isRegistered(name: SymbolicName) = embedding.containsKey(name)
+
+    fun hasDetails(name: SymbolicName) = embeddingDetails.containsKey(name)
+
+    fun allDetails(): List<ClassEmbeddingDetails> = embedding.values.map { details(it.name) }
+    fun details(name: SymbolicName): ClassEmbeddingDetails = embeddingDetails.getOrPut(name)
     {
         ClassEmbeddingDetails(
-            type = types[name]!!,
+            type = embedding[name]!!,
             fields = (classToFields[name] ?: emptySet()).associate { it.first to it.second },
             classSuperTypes = lookupSuperTypes(name).map {
                 details(it.name)
             },
-            isInterface = interfaceTypes.containsKey(name),
-            preType = types[name]!!,
+            isInterface = interfaceEmbedding.containsKey(name),
+            preType = embedding[name]!!,
             this
         )
     }
