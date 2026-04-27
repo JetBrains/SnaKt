84487fc878eb225e3300248b2e6573637d7b0494
marco <paganma@usi.ch>
2026-04-27 10:26:27 +0200

    Use null for representing unknown locality

--- formver.compiler-plugin/locality/src/org/jetbrains/kotlin/formver/locality/plugin/DeclarationLocalityResolver.kt
+++ formver.compiler-plugin/locality/src/org/jetbrains/kotlin/formver/locality/plugin/DeclarationLocalityResolver.kt
@@ -18,7 +18,7 @@ import org.jetbrains.kotlin.fir.types.coneType
 private inline fun FirTypeRef.resolveLocality(findOwner: () -> FirBasedSymbol<*>?): Locality {
     if (coneType.attributes.locality == null) return Global
 
-    return Local(findOwner() ?: UnknownSymbol)
+    return Local(findOwner())
 }
 
 private fun CheckerContext.findClosestFunction(): FirBasedSymbol<*>? =
