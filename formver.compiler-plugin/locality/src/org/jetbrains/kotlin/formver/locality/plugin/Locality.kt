f0c3035d6cee66aa9a924abf846883802854e780
marco <paganma@usi.ch>
2026-04-27 10:26:27 +0200

    Use null for representing unknown locality

--- formver.compiler-plugin/locality/src/org/jetbrains/kotlin/formver/locality/plugin/Locality.kt
+++ formver.compiler-plugin/locality/src/org/jetbrains/kotlin/formver/locality/plugin/Locality.kt
@@ -9,36 +9,53 @@ import org.jetbrains.kotlin.diagnostics.rendering.Renderer
 import org.jetbrains.kotlin.fir.symbols.FirBasedSymbol
 import org.jetbrains.kotlin.fir.symbols.impl.FirCallableSymbol
 
+/**
+ * Symbolic locality value.
+ */
 sealed interface Locality
 
+/**
+ * Symbolic locality value for a global reference.
+ */
 data object Global : Locality
 
+/**
+ * Symbolic locality value for a reference that is local to the declaration referred to by `owner`.
+ */
 data class Local(
-    val owner: FirBasedSymbol<*>
+    val owner: FirBasedSymbol<*>?
 ) : Locality
 
-object UnknownSymbol : FirBasedSymbol<FirDeclaration>(), SyntheticSymbol
-
+/**
+ * Returns `true` if `this` locality accepts [other], `false` otherwise.
+ */
 fun Locality.accepts(other: Locality): Boolean {
     return when (this) {
         Global -> other == Global
         is Local ->
             when (other) {
                 Global -> true
-                is Local -> owner == UnknownSymbol || owner == other.owner
+                is Local -> owner == null || owner == other.owner
             }
     }
 }
 
+/**
+ * Merges `this` locality with [other]. If both `this` and [other] are local to different declarations the result will
+ * be `Local(null)` (local to unknown).
+ */
 fun Locality.union(other: Locality): Locality {
     return when {
         this == other -> this
         this is Global -> other
         other is Global -> this
-        else -> Local(UnknownSymbol)
+        else -> Local(null)
     }
 }
 
+/**
+ * Renders a locality value for diagnostics.
+ */
 val LocalityRenderer = Renderer<Locality> { locality ->
     when (locality) {
         Global -> "'global'"
