0085b58b9e98eebc9e2bea6491853e59db7ed9bd
marco <paganma@usi.ch>
2026-05-11 16:17:49 +0200

    fixup! Improve locality inference

---
+++ formver.compiler-plugin/locality/src/org/jetbrains/kotlin/formver/locality/plugin/VariableLocalityResolver.kt
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2010-2026 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.formver.locality.plugin
+
+import org.jetbrains.kotlin.KtFakeSourceElementKind
+import org.jetbrains.kotlin.fir.analysis.checkers.context.CheckerContext
+import org.jetbrains.kotlin.fir.declarations.FirVariable
+import org.jetbrains.kotlin.fir.expressions.FirExpression
+import org.jetbrains.kotlin.fir.types.FirImplicitTypeRef
+import org.jetbrains.kotlin.fir.types.FirTypeRef
+import org.jetbrains.kotlin.fir.types.coneType
+
+private fun FirTypeRef.isImplicit(): Boolean =
+    this is FirImplicitTypeRef ||
+            source?.kind == KtFakeSourceElementKind.ImplicitTypeRef
+
+context(context: CheckerContext)
+fun FirVariable.resolveLocality(): LocalityAttribute? =
+    resolveLocality { it.resolveLocality() }
+
+internal fun FirVariable.resolveLocality(
+    resolveExpressionLocality: (FirExpression) -> LocalityAttribute?
+): LocalityAttribute? {
+    if (!returnTypeRef.isImplicit()) return returnTypeRef.coneType.locality
+
+    val typeLocality = returnTypeRef.coneType.locality
+
+    return initializer?.let(resolveExpressionLocality) ?: typeLocality
+}
