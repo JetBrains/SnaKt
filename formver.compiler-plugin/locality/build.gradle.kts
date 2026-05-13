51f21a88312b867c12ccc8e5c5ebbf4ec0346509
Ramon.Wick <ramon.wick@jetbrains.com>
2026-05-13 12:03:00 +0200

    update build + CI/CD

--- formver.compiler-plugin/locality/build.gradle.kts
+++ formver.compiler-plugin/locality/build.gradle.kts
@@ -1,10 +1,8 @@
 plugins {
     kotlin("jvm")
-}
-
-dependencies {
-    compileOnly(kotlin("compiler"))
-    compileOnly("org.jetbrains.kotlinx:kotlinx-collections-immutable-jvm:0.3.7")
+    `java-test-fixtures`
+    id("com.github.gmazzo.buildconfig")
+    idea
 }
 
 sourceSets {
@@ -12,8 +10,86 @@ sourceSets {
         java.srcDirs("src")
         resources.srcDir("resources")
     }
-    test {
-        java.setSrcDirs(emptyList<String>())
-        resources.setSrcDirs(emptyList<String>())
+    testFixtures {
+        java.setSrcDirs(listOf("test-fixtures"))
     }
+    test {
+        java.setSrcDirs(listOf("test", "test-gen"))
+        resources.setSrcDirs(listOf("testData"))
+    }
+}
+
+dependencies {
+    compileOnly(kotlin("compiler"))
+    compileOnly("org.jetbrains.kotlinx:kotlinx-collections-immutable-jvm:0.3.7")
+    testFixturesApi(kotlin("test-junit5"))
+    testFixturesApi(kotlin("compiler-internal-test-framework"))
+    testFixturesApi(kotlin("compiler"))
+    implementation(project(":formver.common"))
+
+
+    testRuntimeOnly("junit:junit:4.13.2")
+    testRuntimeOnly(kotlin("reflect"))
+    testRuntimeOnly(kotlin("test"))
+    testRuntimeOnly(kotlin("script-runtime"))
+    testRuntimeOnly(kotlin("annotations-jvm"))
+}
+
+idea {
+    module.generatedSourceDirs.add(projectDir.resolve("test-gen"))
+}
+
+val annotationsRuntimeClasspath: Configuration by configurations.creating { isTransitive = false }
+
+fun Test.configureFormverTest() {
+    dependsOn(annotationsRuntimeClasspath)
+
+    useJUnitPlatform()
+    workingDir = rootDir
+
+    systemProperty("annotationsRuntime.classpath", annotationsRuntimeClasspath.asPath)
+
+    // Properties required to run the internal test framework.
+    setLibraryProperty("org.jetbrains.kotlin.test.kotlin-stdlib", "kotlin-stdlib")
+    setLibraryProperty("org.jetbrains.kotlin.test.kotlin-stdlib-jdk8", "kotlin-stdlib-jdk8")
+    setLibraryProperty("org.jetbrains.kotlin.test.kotlin-reflect", "kotlin-reflect")
+    setLibraryProperty("org.jetbrains.kotlin.test.kotlin-test", "kotlin-test")
+    setLibraryProperty("org.jetbrains.kotlin.test.kotlin-script-runtime", "kotlin-script-runtime")
+    setLibraryProperty("org.jetbrains.kotlin.test.kotlin-annotations-jvm", "kotlin-annotations-jvm")
+
+    systemProperty("idea.ignore.disabled.plugins", "true")
+
+    systemProperty("idea.home.path", rootDir)
+
+    project.findProperty("kotlin.test.update.test.data")?.let {
+        systemProperty("kotlin.test.update.test.data", it)
+    }
+
+}
+
+val generateTests by tasks.registering(JavaExec::class) {
+    inputs.dir(layout.projectDirectory.dir("testData"))
+        .withPropertyName("testData")
+        .withPathSensitivity(PathSensitivity.RELATIVE)
+    outputs.dir(layout.projectDirectory.dir("test-gen"))
+        .withPropertyName("generatedTests")
+
+    classpath = sourceSets.testFixtures.get().runtimeClasspath
+    mainClass.set("org.jetbrains.kotlin.formver.plugin.GenerateTestsKt")
+    workingDir = rootDir
+}
+
+// ./gradlew test — normal mode (full verification)
+tasks.test {
+    configureFormverTest()
+}
+
+fun Test.setLibraryProperty(propName: String, jarName: String) {
+    val path = project.configurations
+        .testRuntimeClasspath.get()
+        .files
+        .find { """$jarName-\d.*jar""".toRegex().matches(it.name) }
+        ?.absolutePath
+        ?: return
+    systemProperty(propName, path)
 }
