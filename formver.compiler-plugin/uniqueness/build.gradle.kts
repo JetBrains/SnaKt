db213dc20fbd04872d09b439ceadb21eea59619d
marco <paganma@usi.ch>
2026-05-18 17:31:16 +0200

    Add kotlinx-collections dependency

--- formver.compiler-plugin/uniqueness/build.gradle.kts
+++ formver.compiler-plugin/uniqueness/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
 
 dependencies {
     compileOnly(kotlin("compiler"))
+    compileOnly("org.jetbrains.kotlinx:kotlinx-collections-immutable-jvm:0.3.7")
     implementation(project(":formver.compiler-plugin:locality"))
 }
 
