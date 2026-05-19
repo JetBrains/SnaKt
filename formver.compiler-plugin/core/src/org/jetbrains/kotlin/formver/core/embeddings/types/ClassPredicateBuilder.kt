3c3fd12f0335125a3ed535e1031718f5a14d2270
Ramon.Wick <ramon.wick@jetbrains.com>
2026-05-19 17:11:49 +0200

    Updated the ClassPredicateBuilder to support handling of function-fields

--- formver.compiler-plugin/core/src/org/jetbrains/kotlin/formver/core/embeddings/types/ClassPredicateBuilder.kt
+++ formver.compiler-plugin/core/src/org/jetbrains/kotlin/formver/core/embeddings/types/ClassPredicateBuilder.kt
@@ -8,8 +8,9 @@ package org.jetbrains.kotlin.formver.core.embeddings.types
 import org.jetbrains.kotlin.formver.core.conversion.AccessPolicy
 import org.jetbrains.kotlin.formver.core.conversion.TypeResolver
 import org.jetbrains.kotlin.formver.core.embeddings.expression.*
+import org.jetbrains.kotlin.formver.core.embeddings.properties.BackingFieldGetter
 import org.jetbrains.kotlin.formver.core.embeddings.properties.FieldEmbedding
-import org.jetbrains.kotlin.formver.core.embeddings.properties.UserFieldEmbedding
+import org.jetbrains.kotlin.formver.core.embeddings.properties.PropertyEmbedding
 import org.jetbrains.kotlin.formver.core.linearization.pureToViper
 import org.jetbrains.kotlin.formver.core.names.DispatchReceiverName
 import org.jetbrains.kotlin.formver.viper.SymbolicName
@@ -18,8 +19,8 @@ import org.jetbrains.kotlin.formver.viper.ast.Predicate
 import org.jetbrains.kotlin.utils.addIfNotNull
 
 internal class ClassPredicateBuilder private constructor(
-    typeEmbedding: TypeEmbedding,
-    val fields: List<FieldEmbedding>,
+    val typeEmbedding: TypeEmbedding,
+    val properties: List<PropertyEmbedding>,
     val classSuperTypes: List<ClassTypeEmbedding>
 ) {
     private val subject = PlaceholderVariableEmbedding(DispatchReceiverName, typeEmbedding)
@@ -35,7 +36,7 @@ internal class ClassPredicateBuilder private constructor(
             val typeEmbedding = ctx.lookupClassTypeEmbedding(name)!!
             val builder = ClassPredicateBuilder(
                 TypeEmbedding(typeEmbedding, TypeEmbeddingFlags(nullable = false)),
-                ctx.lookupClassFields(name),
+                ctx.lookupClassProperties(name),
                 ctx.lookupSuperTypes(name)
             )
             builder.action()
@@ -47,11 +48,14 @@ internal class ClassPredicateBuilder private constructor(
         }
     }
 
-    fun forEachField(action: FieldAssertionsBuilder.() -> Unit) =
-        fields
-            .filterIsInstance<UserFieldEmbedding>()
-            .forEach { field ->
-                val builder = FieldAssertionsBuilder(subject, field)
+    fun includeSubTypeInvariants() = body.add(
+        SubTypeInvariantEmbedding(typeEmbedding).fillHole(subject)
+    )
+
+    fun forEachPropertyField(action: PropertyAssertionsBuilder.() -> Unit) =
+        properties
+            .forEach { property ->
+                val builder = PropertyAssertionsBuilder(subject, property)
                 builder.action()
                 body.addAll(builder.toAssertionsList())
             }
@@ -64,26 +68,58 @@ internal class ClassPredicateBuilder private constructor(
         }
 }
 
-class FieldAssertionsBuilder(private val subject: VariableEmbedding, private val field: UserFieldEmbedding) {
+class PropertyAssertionsBuilder(private val subject: VariableEmbedding, private val property: PropertyEmbedding) {
     private val assertions = mutableListOf<ExpEmbedding>()
     fun toAssertionsList() = assertions.toList()
 
-    val isAlwaysReadable = field.accessPolicy == AccessPolicy.ALWAYS_READABLE
-    val isUnique = field.isUnique
+    val isUnique = property.isUnique
+
+    context(ctx: TypeResolver)
+    private fun getPlainValue() = when (val getter = property.getter!!) {
+        is BackingFieldGetter -> PrimitiveFieldAccess(subject, getter.field)
+        else -> getter.getValueSimple(subject, ctx)
+    }
+
+
+    context(ctx: TypeResolver)
+    fun forType(action: TypeInvariantsBuilder.() -> Unit) {
+        val builder = TypeInvariantsBuilder(property.type)
+        builder.action()
+        assertions.addAll(builder.toInvariantsList().fillHoles(getPlainValue()))
+    }
+
+    fun forBackingField(action: BackingFieldAssertionsBuilder.() -> Unit) {
+        (property.getter as? BackingFieldGetter)?.field?.let { field ->
+            val builder = BackingFieldAssertionsBuilder(subject, field)
+            builder.action()
+            assertions.addAll(builder.toAssertionsList())
+        }
+    }
+
+    context(ctx: TypeResolver)
+    fun addEqualsGuarantee(block: ExpEmbedding.() -> ExpEmbedding) {
+        assertions.add(EqCmp(property.getter!!.getValue(subject, ctx), subject.block()))
+    }
+}
+
+class BackingFieldAssertionsBuilder(private val subject: VariableEmbedding, private val field: FieldEmbedding) {
+    private val assertions = mutableListOf<ExpEmbedding>()
+
+    val isAlwaysWriteable = field.accessPolicy == AccessPolicy.ALWAYS_WRITEABLE
+
+    fun toAssertionsList() = assertions.toList()
+
+    fun addAccessPermissions(perm: PermExp) =
+        assertions.add(FieldAccessTypeInvariantEmbedding(field, perm).fillHole(subject))
+
 
     fun forType(action: TypeInvariantsBuilder.() -> Unit) {
         val builder = TypeInvariantsBuilder(field.type)
         builder.action()
         assertions.addAll(builder.toInvariantsList().fillHoles(PrimitiveFieldAccess(subject, field)))
     }
-
-    fun addAccessPermissions(perm: PermExp) =
-        assertions.add(FieldAccessTypeInvariantEmbedding(field, perm).fillHole(subject))
-
-    fun addEqualsGuarantee(block: ExpEmbedding.() -> ExpEmbedding) {
-        assertions.add(FieldEqualsInvariant(field, subject.block()).fillHole(subject))
-    }
 }
+
 
 class TypeInvariantsBuilder(private val type: TypeEmbedding) {
     private val invariants = mutableListOf<TypeInvariantEmbedding>()
