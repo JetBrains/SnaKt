57ef18fca13f6c087664083fbb0c81efb4fe5dcc
marco <paganma@usi.ch>
2026-05-20 14:32:00 +0200

    Simplify implicit type reference detection

--- formver.compiler-plugin/locality/src/org/jetbrains/kotlin/formver/locality/plugin/VariableLocalityResolver.kt
+++ formver.compiler-plugin/locality/src/org/jetbrains/kotlin/formver/locality/plugin/VariableLocalityResolver.kt
@@ -7,16 +7,24 @@ package org.jetbrains.kotlin.formver.locality.plugin
 
 import org.jetbrains.kotlin.KtFakeSourceElementKind
 import org.jetbrains.kotlin.fir.analysis.checkers.context.CheckerContext
-import org.jetbrains.kotlin.fir.declarations.FirVariable
 import org.jetbrains.kotlin.fir.symbols.impl.FirVariableSymbol
-import org.jetbrains.kotlin.fir.types.FirImplicitTypeRef
 import org.jetbrains.kotlin.fir.types.FirTypeRef
-import org.jetbrains.kotlin.fir.types.coneType
 
 private fun FirTypeRef.isImplicit(): Boolean =
-    this is FirImplicitTypeRef ||
-            source?.kind == KtFakeSourceElementKind.ImplicitTypeRef
+    source?.kind == KtFakeSourceElementKind.ImplicitTypeRef
 
+/**
+ * Resolves the locality of a variable symbol.
+ *
+ * If the locality of a variable is explicitly specified, it is returned directly. Otherwise it is inferred from the
+ * initializer. For example:
+ * val x: local Any = ... // x is assumed to be local
+ * val x: Any = ... // x is assumed to be global
+ * val x = ... // locality of x is inferred from the initializer
+ *
+ * Note that, for the last case, solely relying on the `LocalityAttribute` of `x` is not enough, as the attribute may be
+ * overridden by a cast.
+ */
 context(context: CheckerContext)
 fun FirVariableSymbol<*>.resolveLocality(): Locality {
     if (!resolvedReturnTypeRef.isImplicit()) return resolvedReturnTypeRef.coneType.locality
