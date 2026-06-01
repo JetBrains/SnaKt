3a2df498cf6b8cc489e56cfc4d8ee0aebfa109a6
marco <paganma@usi.ch>
2026-06-01 14:22:25 +0200

    Use UnifyingExpressionTypeResolver for ExpressionUniquenessResolver

--- formver.compiler-plugin/uniqueness/src/org/jetbrains/kotlin/formver/uniqueness/plugin/ExpressionUniquenessResolver.kt
+++ formver.compiler-plugin/uniqueness/src/org/jetbrains/kotlin/formver/uniqueness/plugin/ExpressionUniquenessResolver.kt
@@ -8,6 +8,7 @@ package org.jetbrains.kotlin.formver.uniqueness.plugin
 import kotlinx.collections.immutable.persistentMapOf
 import org.jetbrains.kotlin.fir.FirSession
 import org.jetbrains.kotlin.fir.analysis.checkers.context.CheckerContext
+import org.jetbrains.kotlin.fir.caches.firCachesFactory
 import org.jetbrains.kotlin.fir.expressions.FirExpression
 import org.jetbrains.kotlin.fir.expressions.FirFunctionCall
 import org.jetbrains.kotlin.fir.expressions.FirLiteralExpression
@@ -15,26 +16,17 @@ import org.jetbrains.kotlin.fir.expressions.FirPropertyAccessExpression
 import org.jetbrains.kotlin.fir.expressions.FirReturnExpression
 import org.jetbrains.kotlin.fir.expressions.FirThisReceiverExpression
 import org.jetbrains.kotlin.fir.expressions.FirThrowExpression
-import org.jetbrains.kotlin.fir.expressions.unwrapExpression
 import org.jetbrains.kotlin.fir.extensions.FirExtensionSessionComponent
 import org.jetbrains.kotlin.fir.references.symbol
 import org.jetbrains.kotlin.fir.symbols.impl.FirConstructorSymbol
+import org.jetbrains.kotlin.fir.types.isNothing
+import org.jetbrains.kotlin.fir.types.resolvedType
 import org.jetbrains.kotlin.formver.type.plugin.ExpressionTypeResolver
 import org.jetbrains.kotlin.formver.type.plugin.ReturnResultTypeResolver
 import org.jetbrains.kotlin.formver.type.plugin.ThrowExceptionTypeResolver
-import org.jetbrains.kotlin.formver.type.plugin.collectTails
-import org.jetbrains.kotlin.formver.type.plugin.removeCast
+import org.jetbrains.kotlin.formver.type.plugin.UnifyingExpressionTypeResolver
 import org.jetbrains.kotlin.types.ConstantValueKind
 
-/**
- * Terminal resolver that computes the uniqueness of an expression that has no conditional tails.
- *
- * It considers:
- * - The access trie filtered against the current uniqueness environment.
- * - `null` literals are always unique.
- * - Constructor calls are always unique.
- * - Otherwise falls back to [Uniqueness.Shared].
- */
 private object TerminalUniquenessResolver : ExpressionTypeResolver<Uniqueness> {
     context(context: CheckerContext)
     override fun resolveTypeOf(expression: FirExpression): Uniqueness {
@@ -49,59 +41,51 @@ private object TerminalUniquenessResolver : ExpressionTypeResolver<Uniqueness> {
                 if (expression.calleeReference.symbol is FirConstructorSymbol) Uniqueness.Unique
                 else Uniqueness.Shared
 
-            is FirReturnExpression -> Uniqueness.Unique
-
-            is FirThrowExpression -> Uniqueness.Unique
-
             is FirThisReceiverExpression -> {
                 val symbol = expression.calleeReference.symbol ?: return Uniqueness.Shared
                 val accessState = AccessState(false, persistentMapOf(symbol to AccessState(true)))
 
-                accessState.mask(environment).asUniqueness()
+                accessState.project(environment).asUniqueness()
             }
 
             is FirPropertyAccessExpression -> {
-                expression.resolveAccessState().mask(environment)
-                    .asUniqueness()
+                expression.resolveAccessState().project(environment).asUniqueness()
             }
 
-            else -> Uniqueness.Shared
+            else ->
+                if (expression.resolvedType.isNothing) {
+                    Uniqueness.Unique
+                } else {
+                    Uniqueness.Shared
+                }
         }
     }
 }
 
-/**
- * Resolves the uniqueness of an expression using the flow-sensitive uniqueness environment.
- *
- * Because the uniqueness result depends on a flow-sensitive environment that varies per program
- * point, this resolver does **not** cache results at the session level. Instead, it performs a
- * fresh walk through tails on each invocation, delegating terminal resolution to
- * [TerminalUniquenessResolver].
- *
- * NOTE TO SELF: DO NOT CALL THIS FROM THE ANALYZER
- */
-object ExpressionUniquenessResolver : ExpressionTypeResolver<Uniqueness> {
-    context(context: CheckerContext)
-    private fun extractTypeOf(expression: FirExpression): Uniqueness {
-        val unwrapped = expression.unwrapExpression().removeCast()
-        val tails = unwrapped.collectTails()
-
-        return if (!tails.any()) {
-            TerminalUniquenessResolver.resolveTypeOf(unwrapped)
-        } else {
-            tails.map { extractTypeOf(it) }.reduce(UniquenessUnifier::join)
+class ExpressionUniquenessResolver(session: FirSession) :
+    ExpressionTypeResolver<Uniqueness> by UnifyingExpressionTypeResolver(
+        session.firCachesFactory,
+        UniquenessUnifier,
+        TerminalUniquenessResolver
+    ), FirExtensionSessionComponent(session) {
+    companion object : ExpressionTypeResolver<Uniqueness> {
+        fun getFactory(): Factory {
+            return Factory { session -> ExpressionUniquenessResolver(session) }
         }
-    }
 
-    context(context: CheckerContext)
-    override fun resolveTypeOf(expression: FirExpression): Uniqueness =
-        extractTypeOf(expression)
+        context(context: CheckerContext)
+        override fun resolveTypeOf(expression: FirExpression): Uniqueness =
+            context.session.expressionUniquenessResolver.resolveTypeOf(expression)
+    }
 }
+
+private val FirSession.expressionUniquenessResolver: ExpressionUniquenessResolver
+        by FirSession.sessionComponentAccessor()
 
 object ExpressionDefaultUniquenessResolver : ExpressionTypeResolver<Uniqueness> {
     context(context: CheckerContext)
     private fun extractTypeOf(expression: FirExpression): Uniqueness {
-        return expression.resolveAccessState().resolveUniqueness()
+        return expression.resolveAccessState().extractDefaultUniqueness()
     }
 
     context(context: CheckerContext)
