69e74fa3a0dad696b6fe4c1c614669c898891880
Ramon.Wick <ramon.wick@jetbrains.com>
2026-06-02 11:37:50 +0200

    binary_tree test case update

--- formver.compiler-plugin/testData/diagnostics/verification/negative/binary_tree.kt
+++ formver.compiler-plugin/testData/diagnostics/verification/negative/binary_tree.kt
@@ -1,22 +1,24 @@
 // RENDER_PREDICATES
+// WITH_UNIQUENESS
 // FULL_JDK
 
 import org.jetbrains.kotlin.formver.plugin.AlwaysVerify
 import org.jetbrains.kotlin.formver.plugin.Unique
 import org.jetbrains.kotlin.formver.plugin.verify
+import org.jetbrains.kotlin.formver.plugin.NeverConvert
 
-class Node(var data: Int, @Unique val left: Node?, @Unique val right: Node?)
+class Node(var data: Int, val left: @Unique Node?, val right: @Unique Node?)
 
-fun <!VIPER_TEXT!>get_left_val<!>(@Unique n: Node): Int? {
-    return n.left?.data
+fun <!VIPER_TEXT!>get_left_val<!>(n: @Unique Node): Int? {
+    return < !VIPER_VERIFICATION_ERROR!>n.left?.data<!>
 }
 
-// these expressions should all verify - they currently do not due to lack of uniqueness information
+// these expressions should all verify - they currently do not due to lack of correct fold+unfolding
 @AlwaysVerify
 fun <!VIPER_TEXT!>test<!>() {
-    val n = Node(5, Node(4, null, null), Node(3, Node(2, null, null), Node(1, null, null)))
+    val n: @Unique Node = Node(5, Node(4, null, null), Node(3, Node(2, null, null), Node(1, null, null)))
     val expr1 = n.data == 5
-    verify(<!VIPER_VERIFICATION_ERROR!>expr1<!>)
+    verify(expr1)
     val expr2 = n.left?.data == 4
-    verify(expr2)
+    verify(<!VIPER_VERIFICATION_ERROR!>expr2<!>)
 }
