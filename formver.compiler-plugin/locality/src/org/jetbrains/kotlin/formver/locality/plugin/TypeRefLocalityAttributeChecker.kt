0ec8d02dfb34e4ed64b34664587d32f17aecd3de
marco <paganma@usi.ch>
2026-06-11 15:07:09 +0200

    Fix locality attribute checking

--- formver.compiler-plugin/locality/src/org/jetbrains/kotlin/formver/locality/plugin/TypeRefLocalityAttributeChecker.kt
+++ formver.compiler-plugin/locality/src/org/jetbrains/kotlin/formver/locality/plugin/TypeRefLocalityAttributeChecker.kt
@@ -8,34 +8,51 @@ package org.jetbrains.kotlin.formver.locality.plugin
 import org.jetbrains.kotlin.KtFakeSourceElementKind
 import org.jetbrains.kotlin.diagnostics.DiagnosticReporter
 import org.jetbrains.kotlin.diagnostics.reportOn
+import org.jetbrains.kotlin.fir.FirFunctionTypeParameter
 import org.jetbrains.kotlin.fir.analysis.checkers.MppCheckerKind
 import org.jetbrains.kotlin.fir.analysis.checkers.context.CheckerContext
 import org.jetbrains.kotlin.fir.analysis.checkers.type.FirResolvedTypeRefChecker
-import org.jetbrains.kotlin.fir.FirElement
 import org.jetbrains.kotlin.fir.declarations.FirProperty
 import org.jetbrains.kotlin.fir.declarations.FirReceiverParameter
 import org.jetbrains.kotlin.fir.declarations.FirValueParameter
 import org.jetbrains.kotlin.fir.declarations.isLocal
+import org.jetbrains.kotlin.fir.types.FirFunctionTypeRef
 import org.jetbrains.kotlin.fir.types.FirResolvedTypeRef
 
 object TypeRefLocalityAttributeChecker : FirResolvedTypeRefChecker(MppCheckerKind.Common) {
-    private fun FirElement.isValidLocalityTarget(): Boolean =
-        this is FirValueParameter ||
-                this is FirReceiverParameter ||
-                this is FirProperty && isLocal ||
-                source?.kind is KtFakeSourceElementKind.ImplicitTypeArgument
+    private fun CheckerContext.isValidLocalityTypeTarget(): Boolean {
+        val containingElements = containingElements.asReversed()
+        val target = containingElements.getOrNull(1) ?: return false
 
-    private fun CheckerContext.isValidLocalityTarget(): Boolean {
-        val target = containingElements.dropLast(1).lastOrNull()
+        return when(target) {
+            is FirValueParameter, is FirReceiverParameter -> true
+            is FirProperty -> target.isLocal
+            else -> target.source?.kind is KtFakeSourceElementKind.ImplicitTypeArgument
+        }
+    }
 
-        return target?.isValidLocalityTarget() ?: false
+    private fun CheckerContext.isValidLocalityContractTarget(): Boolean {
+        val containingElements = containingElements.asReversed()
+        val target0 = containingElements.getOrNull(0) ?: return false
+        val target1 = containingElements.getOrNull(1) ?: return false
+
+        return when (target1) {
+            is FirFunctionTypeRef ->
+                target0 == target1.receiverTypeRef || target0 in target1.contextParameterTypeRefs
+            is FirFunctionTypeParameter -> {
+                val target2 = containingElements.getOrNull(2)
+
+                target2 is FirFunctionTypeRef && target1 in target2.parameters
+            }
+            else -> false
+        }
     }
 
     context(context: CheckerContext, reporter: DiagnosticReporter)
     override fun check(typeRef: FirResolvedTypeRef) {
         if (typeRef.coneType.attributes.locality == null) return
 
-        if (context.isValidLocalityTarget()) return
+        if (context.isValidLocalityTypeTarget() || context.isValidLocalityContractTarget()) return
 
         reporter.reportOn(typeRef.source, LocalityErrors.INVALID_LOCALITY_TYPE_TARGET)
     }
