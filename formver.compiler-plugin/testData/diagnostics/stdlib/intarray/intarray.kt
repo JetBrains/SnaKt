98657c3a2beec1720ada1a3b374717e20fd7a300
Ramon.Wick <ramon.wick@jetbrains.com>
2026-06-23 11:29:37 +0200

    Add tests and Viper generation for `IntArray`

---
+++ formver.compiler-plugin/testData/diagnostics/stdlib/intarray/intarray.kt
@@ -0,0 +1,40 @@
+// FULL_JDK
+// WITH_STDLIB
+// RENDER_PREDICATES
+
+import org.jetbrains.kotlin.formver.plugin.*
+
+@AlwaysVerify
+fun <!VIPER_TEXT!>testSize<!>(@Unique @Borrowed a: IntArray) {
+    val n = a.size
+    verify(n == a.size)
+}
+
+@AlwaysVerify
+fun <!VIPER_TEXT!>testGet<!>(@Unique @Borrowed a: IntArray) {
+    preconditions {
+        a.size > 0
+    }
+    val x = a[0]
+    verify(x == a[0])
+}
+
+@AlwaysVerify
+fun <!VIPER_TEXT!>testSet<!>(@Unique @Borrowed a: IntArray) {
+    preconditions {
+        a.size > 0
+    }
+    a[0] = 42
+    verify(a[0] == 42)
+}
+
+@AlwaysVerify
+fun <!VIPER_TEXT!>testSizePreservedAfterSet<!>(@Unique @Borrowed a: IntArray) {
+    preconditions {
+        a.size > 1
+    }
+    val before = a.size
+    a[1] = 7
+    verify(a.size == before)
+    verify(a[1] == 7)
+}
