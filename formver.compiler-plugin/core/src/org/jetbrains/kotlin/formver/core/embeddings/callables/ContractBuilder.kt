JetBrains/SnaKt

commit 556e9eec9ec366dd07f5fec781f9c48da4018fb4

Refactor Callable Embedding Type Hierarchy (#169)
This PR resolves the issue with recursive contracts. In the process the
embedding process was cleaned up. There are also some changes in the
type hierarchy of signatures and callables.

## Changes in the Type Hierarchy
Now, the signatures are callable ones they are on the
`NonInlineNamedFunctionSignature`/`InlineFunctionSignature` level.
- For **Non-Inline**, the necessary things are the name and signature.
Here a contract is not required.
- For **Inline**, the necessary things are the name, signature as well
as the contract. Because ideally, when calling an inline function we
want to `assert` and `assume` the pre+post condition of the inlined
function.

## Changes in the ProgramConverter
To mitigate the recursion problem the `callable` map was introduced.
This stores all the functions that can be called. Furthermore, instead
of having the pure and impure functions, we just have
`fullFunctionSignatures` which stores functions that have their contract
already embedded. The pure vs impure separation is not necessary, since
every signature has a boolean flag.

formver.compiler-plugin/core/src/org/jetbrains/kotlin/formver/core/embeddings/callables/ContractBuilder.kt
@@ -1,0 +1,149 @@
+package org.jetbrains.kotlin.formver.core.embeddings.callables
+
+import org.jetbrains.kotlin.formver.common.SnaktInternalException
+import org.jetbrains.kotlin.formver.core.conversion.TypeResolver
+import org.jetbrains.kotlin.formver.core.conversion.stdLibPostconditions
+import org.jetbrains.kotlin.formver.core.conversion.stdLibPreconditions
+import org.jetbrains.kotlin.formver.core.embeddings.expression.AccEmbedding
+import org.jetbrains.kotlin.formver.core.embeddings.expression.ExpEmbedding
+import org.jetbrains.kotlin.formver.core.embeddings.expression.PredicateAccessPermissions
+import org.jetbrains.kotlin.formver.core.embeddings.expression.VariableEmbedding
+import org.jetbrains.kotlin.formver.core.purity.preorder
+
+
+// Container for the generated conditions
+data class FunctionContract(
+    val preconditions: List<ExpEmbedding>, val postconditions: List<ExpEmbedding>
+)
+
+
+fun NamedFunctionSignature.buildConditions(
+    typeResolver: TypeResolver, block: FunctionConditionBuilder.() -> Unit
+): FunctionContract {
+    return FunctionConditionBuilder(this, typeResolver).apply(block).build()
+}
+
+class FunctionConditionBuilder(
+    private val signature: NamedFunctionSignature, private val typeResolver: TypeResolver
+) {
+    private val preconditions = mutableListOf<ExpEmbedding>()
+    private val postconditions = mutableListOf<ExpEmbedding>()
+
+    fun preconditions(block: PreconditionScope.() -> Unit) {
+        PreconditionScope(signature, typeResolver, preconditions).apply(block)
+    }
+
+    fun postconditions(block: PostconditionScope.() -> Unit) {
+        PostconditionScope(signature, typeResolver, postconditions).apply(block)
+    }
+
+
+    fun addPreconditions(list: List<ExpEmbedding>) = preconditions.addAll(list)
+    fun addPostconditions(list: List<ExpEmbedding>) = postconditions.addAll(list)
+
+    fun userFunctionPreconditions() {
+        preconditions {
+            args {
+                pureInvariants()
+                provenInvariants()
+                accessInvariants()
+                if (variable.isUnique) uniquePredicateInvariants()
+            }
+            stdLib()
+        }
+    }
+
+    fun userFunctionPostcondition() {
+        postconditions {
+            args {
+                accessInvariants()
+                pureInvariants()
+                if (variable.isBorrowed && variable.isUnique) uniquePredicateInvariants()
+            }
+            returns {
+                pureInvariants()
+                provenInvariants()
+                if (!signature.isPure) {
+                    accessInvariants()
+                    if (signature.callableType.returnsUnique) {
+                        uniquePredicateInvariants()
+                    }
+                }
+            }
+            stdLib()
+        }
+    }
+
+    fun userFunctionContract() {
+        userFunctionPreconditions()
+        userFunctionPostcondition()
+    }
+
+    private fun checkNoPermInPurePostcondition() {
+        if (signature.isPure) {
+            postconditions.forEach { postcondition ->
+                val isValid =
+                    postcondition.preorder()
+                        .all { it.first !is AccEmbedding && it.first !is PredicateAccessPermissions }
+                if (!isValid) throw SnaktInternalException(
+                    signature.symbol?.source,
+                    "Postcondition tries to acquire permissions, which is not allowed in a function"
+                )
+            }
+        }
+    }
+
+    fun build(): FunctionContract {
+        checkNoPermInPurePostcondition()
+        return FunctionContract(preconditions, postconditions)
+    }
+}
+
+class PreconditionScope(
+    private val signature: NamedFunctionSignature,
+    private val typeResolver: TypeResolver,
+    private val list: MutableList<ExpEmbedding>
+) {
+    fun args(block: VariableScope.() -> Unit) {
+        signature.formalArgs.forEach { variable -> VariableScope(variable, list, typeResolver).apply(block) }
+    }
+
+    fun stdLib() {
+        list.addAll(signature.stdLibPreconditions(typeResolver))
+    }
+}
+
+
+class VariableScope(
+    val variable: VariableEmbedding,
+    private val list: MutableList<ExpEmbedding>,
+    private val typeResolver: TypeResolver
+) {
+    fun pureInvariants() = list.addAll(variable.pureInvariants())
+    fun accessInvariants() = list.addAll(variable.accessInvariants(typeResolver))
+    fun provenInvariants() = list.addAll(variable.provenInvariants())
+
+    fun uniquePredicateInvariants() {
+        variable.type.uniquePredicateAccessInvariant(typeResolver)?.fillHole(variable)?.let { inv ->
+            list.add(inv)
+        }
+    }
+}
+
+class PostconditionScope(
+    private val signature: NamedFunctionSignature,
+    private val typeResolver: TypeResolver,
+    private val list: MutableList<ExpEmbedding>
+) {
+    fun args(block: VariableScope.() -> Unit) {
+        signature.formalArgs.forEach { variable -> VariableScope(variable, list, typeResolver).apply(block) }
+    }
+
+    fun returns(block: VariableScope.() -> Unit) {
+        VariableScope(signature.returns, list, typeResolver).apply(block)
+    }
+
+    fun stdLib() {
+        list.addAll(signature.stdLibPostconditions(signature.returns, typeResolver))
+    }
+}

formver.compiler-plugin/core/src/org/jetbrains/kotlin/formver/core/FirUtils.kt
@@ -84,6 +84,8 @@ fun FirAnnotationContainer.isUnique(session: FirSession) = hasAnnotation(annotat
 
 fun FirAnnotationContainer.isBorrowed(session: FirSession) = hasAnnotation(annotationId("Borrowed"), session)
 
+fun FirFunctionSymbol<*>.neverConvert(session: FirSession) = hasAnnotation(annotationId("NeverConvert"), session)
+
 fun FirFunctionSymbol<*>.isFormverFunctionNamed(name: String) =
     this is FirNamedFunctionSymbol && callableId == formverCallableId(className = null, name)
 

formver.compiler-plugin/core/src/org/jetbrains/kotlin/formver/core/conversion/ContractDescriptionConversionVisitor.kt
@@ -9,6 +9,7 @@ import org.jetbrains.kotlin.contracts.description.*
 import org.jetbrains.kotlin.fir.contracts.description.ConeContractConstantValues
 import org.jetbrains.kotlin.fir.diagnostics.ConeDiagnostic
 import org.jetbrains.kotlin.fir.symbols.FirBasedSymbol
+import org.jetbrains.kotlin.fir.symbols.impl.FirFunctionSymbol
 import org.jetbrains.kotlin.fir.types.ConeKotlinType
 import org.jetbrains.kotlin.formver.core.effects
 import org.jetbrains.kotlin.formver.core.embeddings.SourceRole
@@ -24,9 +25,10 @@ import org.jetbrains.kotlin.formver.core.embeddings.expression.*
 class ContractDescriptionConversionVisitor(
     private val ctx: ProgramConversionContext,
     private val signature: NamedFunctionSignature,
+    private val symbol: FirFunctionSymbol<*>
 ) : KtContractDescriptionVisitor<ExpEmbedding, Unit, ConeKotlinType, ConeDiagnostic>() {
     fun getPostconditions(): List<ExpEmbedding> =
-        signature.symbol.effects.map {
+        symbol.effects.map {
             it.effect.accept(this, Unit).withPosition(it.source)
         }.filter { it != BooleanLit(true) }
 
@@ -174,7 +176,7 @@ class ContractDescriptionConversionVisitor(
     private fun KtValueParameterReference<ConeKotlinType, ConeDiagnostic>.getTargetParameter(): FirBasedSymbol<*> =
         resolveByIndex(
             parameterIndex,
-            { signature.symbol }) { signature.symbol.valueParameterSymbols[it] }
+            { symbol }) { symbol.valueParameterSymbols[it] }
 
     // If both receivers are present only references to extension receivers will be allowed in contract.
     private fun allowedReceiver() = signature.extensionReceiver ?: signature.dispatchReceiver

formver.compiler-plugin/core/src/org/jetbrains/kotlin/formver/core/conversion/ProgramConversionContext.kt
@@ -5,6 +5,7 @@
 
 package org.jetbrains.kotlin.formver.core.conversion
 
+import org.jetbrains.kotlin.fir.FirSession
 import org.jetbrains.kotlin.fir.expressions.FirExpression
 import org.jetbrains.kotlin.fir.symbols.impl.FirFunctionSymbol
 import org.jetbrains.kotlin.fir.symbols.impl.FirPropertySymbol
@@ -13,10 +14,10 @@ import org.jetbrains.kotlin.fir.types.resolvedType
 import org.jetbrains.kotlin.formver.common.PluginConfiguration
 import org.jetbrains.kotlin.formver.core.diagnostics.ErrorCollectionContext
 import org.jetbrains.kotlin.formver.core.embeddings.callables.CallableEmbedding
-import org.jetbrains.kotlin.formver.core.embeddings.callables.FunctionSignature
-import org.jetbrains.kotlin.formver.core.embeddings.callables.PureUserFunctionEmbedding
+import org.jetbrains.kotlin.formver.core.embeddings.callables.NamedFunctionSignature
 import org.jetbrains.kotlin.formver.core.embeddings.expression.AnonymousBuiltinVariableEmbedding
 import org.jetbrains.kotlin.formver.core.embeddings.expression.AnonymousVariableEmbedding
+import org.jetbrains.kotlin.formver.core.embeddings.expression.ExpEmbedding
 import org.jetbrains.kotlin.formver.core.embeddings.expression.VariableEmbedding
 import org.jetbrains.kotlin.formver.core.embeddings.properties.PropertyEmbedding
 import org.jetbrains.kotlin.formver.core.embeddings.types.FunctionTypeEmbedding
@@ -26,7 +27,10 @@ import org.jetbrains.kotlin.formver.core.names.TryExitLabelName
 import org.jetbrains.kotlin.formver.viper.NameResolver
 
 interface ProgramConversionContext : ErrorCollectionContext {
+
     val config: PluginConfiguration
+
+    val session: FirSession
 
     val whileIndexProducer: SimpleFreshEntityProducer<Int>
     val catchLabelNameProducer: SimpleFreshEntityProducer<CatchLabelName>
@@ -41,14 +45,14 @@ interface ProgramConversionContext : ErrorCollectionContext {
     val convertedBodyResolver: ConvertedBodyResolver
     val linearizedBodyResolver: LinearizedBodyResolver
 
-    fun embedFunction(symbol: FirFunctionSymbol<*>): CallableEmbedding
-    fun embedPureFunction(symbol: FirFunctionSymbol<*>): PureUserFunctionEmbedding
     fun embedAnyFunction(symbol: FirFunctionSymbol<*>): CallableEmbedding
-    fun embedFunctionSignature(symbol: FirFunctionSymbol<*>): Pair<ReturnTarget,FunctionSignature>
     fun embedType(type: ConeKotlinType): TypeEmbedding
     fun embedFunctionPretype(symbol: FirFunctionSymbol<*>): FunctionTypeEmbedding
     fun embedType(exp: FirExpression): TypeEmbedding = embedType(exp.resolvedType)
     fun embedProperty(symbol: FirPropertySymbol): PropertyEmbedding
+    fun embedContract(
+        symbol: FirFunctionSymbol<*>, signature: NamedFunctionSignature, returnTarget: ReturnTarget
+    ): Pair<List<ExpEmbedding>, List<ExpEmbedding>>
 
     /**
      * Returns true if the property has default behavior. That is:

formver.compiler-plugin/core/src/org/jetbrains/kotlin/formver/core/conversion/SignatureCreation.kt
@@ -1,0 +1,225 @@
+package org.jetbrains.kotlin.formver.core.conversion
+
+import org.jetbrains.kotlin.KtSourceElement
+import org.jetbrains.kotlin.fir.analysis.checkers.isPrimaryConstructor
+import org.jetbrains.kotlin.fir.declarations.DirectDeclarationsAccess
+import org.jetbrains.kotlin.fir.declarations.utils.correspondingValueParameterFromPrimaryConstructor
+import org.jetbrains.kotlin.fir.resolve.toRegularClassSymbol
+import org.jetbrains.kotlin.fir.symbols.SymbolInternals
+import org.jetbrains.kotlin.fir.symbols.impl.*
+import org.jetbrains.kotlin.fir.types.ConeKotlinType
+import org.jetbrains.kotlin.formver.common.SnaktInternalException
+import org.jetbrains.kotlin.formver.core.embeddings.callables.*
+import org.jetbrains.kotlin.formver.core.embeddings.expression.EqCmp
+import org.jetbrains.kotlin.formver.core.embeddings.expression.FirVariableEmbedding
+import org.jetbrains.kotlin.formver.core.embeddings.expression.PlaceholderVariableEmbedding
+import org.jetbrains.kotlin.formver.core.embeddings.types.FunctionTypeEmbedding
+import org.jetbrains.kotlin.formver.core.isBorrowed
+import org.jetbrains.kotlin.formver.core.isPure
+import org.jetbrains.kotlin.formver.core.isUnique
+import org.jetbrains.kotlin.formver.core.names.*
+import org.jetbrains.kotlin.formver.viper.SymbolicName
+
+data class SignatureWithTarget<out S : FunctionSignature>(
+    val signature: S, val returnTarget: ReturnTarget
+) {
+    /**
+     * Refines the signature by applying the given action to the current signature.
+     * The return target is preserved.
+     *
+     * It should be used to make a signature more specific, e.g., to add pre+post conditions to an existing signature
+     */
+    fun <T : FunctionSignature> refineSignature(action: (SignatureWithTarget<S>) -> T): SignatureWithTarget<T> =
+        SignatureWithTarget(action(this), returnTarget)
+}
+
+@OptIn(DirectDeclarationsAccess::class)
+val FirRegularClassSymbol.propertySymbols: List<FirPropertySymbol>
+    get() = declarationSymbols.filterIsInstance<FirPropertySymbol>()
+
+private fun <R> FirPropertySymbol.withConstructorParam(action: FirPropertySymbol.(FirValueParameterSymbol) -> R): R? =
+    correspondingValueParameterFromPrimaryConstructor?.let { param ->
+        action(param)
+    }
+
+private val FirFunctionSymbol<*>.containingPropertyOrSelf
+    get() = when (this) {
+        is FirPropertyAccessorSymbol -> propertySymbol
+        else -> this
+    }
+
+val FirFunctionSymbol<*>.receiverType: ConeKotlinType?
+    get() = containingPropertyOrSelf.dispatchReceiverType
+
+val FirFunctionSymbol<*>.extensionReceiverType: ConeKotlinType?
+    get() = containingPropertyOrSelf.resolvedReceiverTypeRef?.coneType
+
+context(converter: ProgramConversionContext)
+fun FirFunctionSymbol<*>.toFunctionSignature(): SignatureWithTarget<FunctionSignature> {
+    val dispatchReceiverType = this.receiverType
+    val extensionReceiverType = this.extensionReceiverType
+
+    val returnType = converter.embedType(this.resolvedReturnType)
+
+    val returnTarget = when {
+        this.isPure(converter.session) -> ReturnTarget.createForPureFunction(returnType)
+        else -> converter.returnTargetProducer.getFresh(returnType)
+    }
+
+    val dispatchVariable = dispatchReceiverType?.let {
+        PlaceholderVariableEmbedding(
+            DispatchReceiverName,
+            converter.embedType(it),
+            isUnique = false,
+            isBorrowed = false,
+        )
+    }
+    val extensionVariable = extensionReceiverType?.let {
+        PlaceholderVariableEmbedding(
+            ExtensionReceiverName,
+            converter.embedType(it),
+            this.receiverParameterSymbol?.isUnique(converter.session) ?: false,
+            this.receiverParameterSymbol?.isBorrowed(converter.session) ?: false,
+        )
+    }
+
+    val parameterVariables = this.valueParameterSymbols.map {
+        FirVariableEmbedding(
+            it.embedName(),
+            converter.embedType(it.resolvedReturnType),
+            it,
+            it.isUnique(converter.session),
+            it.isBorrowed(converter.session)
+        )
+    }
+
+    val signature = FunctionSignatureImpl(
+        converter.embedFunctionPretype(this),
+        dispatchVariable,
+        extensionVariable,
+        parameterVariables,
+        returns = returnTarget.variable,
+        isPure = this.isPure(converter.session)
+    )
+
+    return SignatureWithTarget(signature, returnTarget)
+}
+
+context(converter: ProgramConversionContext)
+fun FunctionTypeEmbedding.toGenericAccessorSignature(isPure: Boolean): SignatureWithTarget<FunctionSignature> {
+    val returnTarget = when (isPure) {
+        true -> ReturnTarget.createForPureFunction(returnType)
+        false -> converter.returnTargetProducer.getFresh(returnType)
+    }
+    val extensionVariable = extensionReceiverType?.let { PlaceholderVariableEmbedding(ExtensionReceiverName, it) }
+    val dispatchVariable = dispatchReceiverType?.let { PlaceholderVariableEmbedding(DispatchReceiverName, it) }
+    val parameterVariables =
+        paramTypes.mapIndexed { index, embedding -> PlaceholderVariableEmbedding(AnonymousName(index), embedding) }
+    val signature = FunctionSignatureImpl(
+        this,
+        dispatchVariable,
+        extensionVariable,
+        parameterVariables,
+        returnTarget.variable,
+        isPure
+    )
+    return SignatureWithTarget(signature, returnTarget)
+}
+
+
+context(converter: ProgramConversionContext)
+fun SignatureWithTarget<FunctionSignature>.toNamedSignature(symbol: FirFunctionSymbol<*>): SignatureWithTarget<NamedFunctionSignature> =
+    this.refineSignature { current ->
+        NamedFunctionSignatureImpl(current.signature, symbol.embedName(converter), symbol)
+    }
+
+fun SignatureWithTarget<FunctionSignature>.toNamedSignature(name: SymbolicName): SignatureWithTarget<NamedFunctionSignature> =
+    this.refineSignature { current ->
+        NamedFunctionSignatureImpl(current.signature, name, null)
+    }
+
+
+fun SignatureWithTarget<NamedFunctionSignature>.toNonInlineSignature(symbol: FirFunctionSymbol<*>?): SignatureWithTarget<NonInlineCallable> =
+    this.refineSignature { current ->
+        NonInlineCallableImpl(current.signature, symbol)
+    }
+
+
+context(converter: ProgramConversionContext)
+fun SignatureWithTarget<NonInlineCallable>.toCompleteSignature(symbol: FirFunctionSymbol<*>): SignatureWithTarget<NonInlineFunctionSignature> =
+    when {
+        symbol.isPrimaryConstructor() -> this.toConstructorSignature(symbol)
+        else -> this.toNormalSignature(symbol)
+    }
+
+context(converter: ProgramConversionContext)
+fun SignatureWithTarget<NonInlineCallable>.toCompleteSignature(
+    declarationSource: KtSourceElement?, action: FunctionConditionBuilder.() -> Unit
+): SignatureWithTarget<NonInlineFunctionSignature> = refineSignature { current ->
+    val (preconditions, postconditions) = current.signature.buildConditions(converter.typeResolver, action)
+    NonInlineFunctionSignature(current.signature, preconditions, postconditions, declarationSource)
+}
+
+
+context(converter: ProgramConversionContext)
+fun SignatureWithTarget<NonInlineCallable>.toConstructorSignature(symbol: FirFunctionSymbol<*>): SignatureWithTarget<NonInlineFunctionSignature> =
+    refineSignature { current ->
+        val constructedClassSymbol =
+            symbol.resolvedReturnType.toRegularClassSymbol(converter.session) ?: throw SnaktInternalException(
+                symbol.source, "Constructor does not return a regular class"
+            )
+        val parameterMatching = constructedClassSymbol.propertySymbols.mapNotNull { propertySymbol ->
+            val name = propertySymbol.embedMemberPropertyName(converter)
+            propertySymbol.withConstructorParam { paramSymbol ->
+                converter.typeResolver.lookupDefaultBehavingProperties(name)?.let { paramSymbol to it }
+            }
+        }.toMap()
+
+        val fieldPostconditions = current.signature.params.mapNotNull { param ->
+            require(param is FirVariableEmbedding) { "Constructor parameters must be represented by FirVariableEmbeddings" }
+            parameterMatching[param.symbol]?.let { property ->
+                EqCmp(property.getter!!.getValueSimple(returnTarget.variable, converter.typeResolver), param)
+            }
+        }
+
+        val contract = current.signature.buildConditions(converter.typeResolver) {
+            userFunctionContract()
+            addPostconditions(fieldPostconditions)
+        }
+
+        NonInlineFunctionSignature(current.signature, contract.preconditions, contract.postconditions, symbol.source)
+    }
+
+context(converter: ProgramConversionContext)
+fun SignatureWithTarget<NonInlineCallable>.toNormalSignature(symbol: FirFunctionSymbol<*>): SignatureWithTarget<NonInlineFunctionSignature> =
+    refineSignature { current ->
+        val contract = current.signature.buildConditions(converter.typeResolver) {
+            userFunctionContract()
+            val (preconditions, postconditions) = converter.embedContract(
+                symbol, current.signature, returnTarget
+            )
+            addPreconditions(preconditions)
+            addPostconditions(postconditions)
+        }
+        NonInlineFunctionSignature(current.signature, contract.preconditions, contract.postconditions, symbol.source)
+    }
+
+@OptIn(SymbolInternals::class)
+context(converter: ProgramConversionContext)
+fun SignatureWithTarget<NamedFunctionSignature>.toInlineSignature(symbol: FirFunctionSymbol<*>): SignatureWithTarget<InlineNamedFunction> =
+    this.refineSignature { current ->
+        val body = symbol.fir.body ?: throw SnaktInternalException(symbol.source, "Expected function body, got null")
+        val contract = current.signature.buildConditions(converter.typeResolver) {
+            val (precondition, postcondition) = converter.embedContract(
+                symbol, current.signature, current.returnTarget
+            )
+            userFunctionContract()
+            addPreconditions(precondition)
+            addPostconditions(postcondition)
+        }
+
+        val fullSignature = InlineNamedFunction(
+            current.signature, body, contract.preconditions, contract.postconditions, symbol
+        )
+        fullSignature
+    }

formver.compiler-plugin/core/src/org/jetbrains/kotlin/formver/core/conversion/StmtConversionContext.kt
@@ -17,8 +17,8 @@ import org.jetbrains.kotlin.fir.types.resolvedType
 import org.jetbrains.kotlin.formver.common.SnaktInternalException
 import org.jetbrains.kotlin.formver.core.embeddings.FunctionBodyEmbedding
 import org.jetbrains.kotlin.formver.core.embeddings.LabelEmbedding
-import org.jetbrains.kotlin.formver.core.embeddings.callables.FullNamedFunctionSignature
 import org.jetbrains.kotlin.formver.core.embeddings.callables.FunctionSignature
+import org.jetbrains.kotlin.formver.core.embeddings.callables.NamedFunctionSignatureWithContract
 import org.jetbrains.kotlin.formver.core.embeddings.expression.*
 import org.jetbrains.kotlin.formver.core.embeddings.properties.ClassPropertyAccess
 import org.jetbrains.kotlin.formver.core.embeddings.properties.PropertyAccessEmbedding
@@ -244,7 +244,7 @@ fun StmtConversionContext.insertForAllFunctionCall(
 
 fun StmtConversionContext.convertImpureBody(
     declaration: FirSimpleFunction,
-    signature: FullNamedFunctionSignature,
+    signature: NamedFunctionSignatureWithContract,
     returnTarget: ReturnTarget,
 ): ConvertedMethodBody? {
     val firBody = declaration.body ?: return null

formver.compiler-plugin/core/src/org/jetbrains/kotlin/formver/core/conversion/StmtConversionVisitor.kt
@@ -509,9 +509,10 @@ object StmtConversionVisitor : FirVisitor<ExpEmbedding, StmtConversionContext>()
         data: StmtConversionContext,
     ): ExpEmbedding {
         val function = anonymousFunctionExpression.anonymousFunction
-        val (returnTarget, signature) = data.embedFunctionSignature(function.symbol)
+        val (signature, _) = with(data) { function.symbol.toFunctionSignature() }
         return LambdaExp(signature, function, data, function.symbol.label!!.name)
     }
+
 
     override fun visitTryExpression(tryExpression: FirTryExpression, data: StmtConversionContext): ExpEmbedding {
         val (catchData, tryBody) = data.withCatches(tryExpression.catches) { catchData ->

commit 7627eb7f6ddd901ff5656b24c98d3e37e4c372ed

Resolved Pure Function Permission Issue (#190)
This PR fixes a bug introduced by #169 
In a pure function if a parameter was annotated as unique and borrowed
in the postcondition the unique predicate was added. Viper does not
allow to have permissions in the postcondition of pure functions.

formver.compiler-plugin/core/src/org/jetbrains/kotlin/formver/core/embeddings/callables/ContractBuilder.kt
@@ -58,7 +58,7 @@ class FunctionConditionBuilder(
             args {
                 accessInvariants()
                 pureInvariants()
-                if (variable.isBorrowed && variable.isUnique) uniquePredicateInvariants()
+                if (variable.isBorrowed && variable.isUnique && !signature.isPure) uniquePredicateInvariants()
             }
             returns {
                 pureInvariants()

formver.compiler-plugin/testData/diagnostics/verification/pure_functions/pure_function_with_heap_dependent_expressions.kt
@@ -88,3 +88,6 @@ fun <!VIPER_TEXT!>getNextValueUsingId<!>(node: Node): Int {
     val nextNode = id(node.next)
     return if (nextNode == null) 0 else nextNode.value
 }
+
+@Pure
+fun <!VIPER_TEXT!>testBorrowed<!>(@Unique @Borrowed node: Node): Unit = Unit